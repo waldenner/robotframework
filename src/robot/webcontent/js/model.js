7ce6d87d389e25c536299d908e5e14cf0974af0b
Mikko Korpela <mikko.korpela@gmail.com>
2011-06-08 13:16:38 +0300

    webcontent: test and suite message

---
+++ src/robot/webcontent/js/model.js
@@ -0,0 +1,393 @@
+window.model = function () {
+
+    var STATUS = {
+        pass:"pass",
+        fail:"fail"
+    };
+
+    var KEYWORD_TYPE = {
+        kw: 'KEYWORD',
+        setup:'SETUP',
+        teardown:'TEARDOWN'
+    };
+
+    function Suite(data) {
+        var suite = createModelObject(data);
+        suite.source = data.source;
+        suite.fullname = data.parent ? data.parent.fullname + "." + data.name : data.name;
+        suite.statusText = data.status.status.toUpperCase();
+        setStats(suite, data.statistics);
+        suite.metadata = data.metadata;
+        suite.populateKeywords = createIterablePopulator("Keyword");
+        suite.populateTests = createIterablePopulator("Test");
+        suite.populateSuites = createIterablePopulator("Suite");
+        suite.message = data.message;
+        suite.children = function () {
+            return suite.keywords().concat(suite.tests()).concat(suite.suites());
+        };
+        // TODO: Create message for suite here, not via a function
+        // TODO: It seems failures in suite setup aren't handled at all
+        suite.hasTeardownFailure = function () {
+            return suiteTeardownFailed(suite) || data.status.parentSuiteTeardownFailed;
+        };
+        suite.searchTests = function (predicate) {
+            var tests = [];
+            for (var i = 0; i < this.numberOfSuites; i++)
+                tests = tests.concat(this.suite(i).searchTests(predicate));
+            return tests.concat(util.filter(this.tests(), predicate));
+        };
+        suite.searchTestsByTag = function (tag) {
+            return suite.searchTests(function (test) {
+                return containsTag(test.tags, tag.label, tag.info == 'combined');
+            });
+        };
+        suite.findSuiteByName = function (name) {
+            return findSuiteByName(suite, name);
+        };
+        suite.allTests = function () {
+            return suite.searchTests(function (test) {
+                return true;
+            });
+        };
+        suite.criticalTests = function () {
+            return suite.searchTests(function (test) {
+                return test.isCritical;
+            });
+        };
+        return suite;
+    }
+
+    function containsTag(testTags, tagname, isCombined) {
+        testTags = util.map(testTags, util.normalize);
+        if (!isCombined)
+            return util.contains(testTags, util.normalize(tagname));
+        if (tagname.indexOf(' & ') != -1) {
+            var tagnames = tagname.split(' & ');
+            return util.all(util.map(tagnames, function (name) {
+                return containsTag(testTags, name, true);
+            }));
+        }
+        if (tagname.indexOf(' NOT ') != -1) {
+            var tagnames = tagname.split(' NOT ');
+            var required = tagnames[0];
+            var notAllowed = tagnames.slice(1);
+            return containsTag(testTags, required, true) &&
+                    util.all(util.map(notAllowed, function (name) {
+                        return !containsTag(testTags, name, true);
+                    }))
+        }
+        var matcher = util.Matcher(tagname)
+        return util.any(util.map(testTags, matcher.matches));
+    }
+
+    function findSuiteByName(suite, name) {
+        if (suite.fullname == name)
+            return suite;
+        var subSuites = suite.suites();
+        for (var i = 0; i < subSuites.length; i++) {
+            var match = findSuiteByName(subSuites[i], name);
+            if (match)
+                return match;
+        }
+        return null;
+    }
+
+    function suiteTeardownFailed(suite) {
+        if (suite.numberOfKeywords) {
+            var kw = suite.keyword(suite.numberOfKeywords - 1);
+            if (kw.type == KEYWORD_TYPE.teardown)
+                return kw.status == STATUS.fail;
+        }
+        return false;
+    }
+
+    function setStats(suite, stats) {
+        for (var name in stats) {
+            suite[name] = stats[name];
+        }
+        // TODO: move to templates
+        if (suite.totalFailed == 0)
+            suite.totalFailureClass = 'pass';
+        else
+            suite.totalFailureClass = 'fail';
+        if (suite.criticalFailed == 0)
+            suite.criticalFailureClass = 'pass';
+        else
+            suite.criticalFailureClass = 'fail';
+    }
+
+    function createModelObject(data) {
+        var obj = {};
+        obj.name = data.name;
+        obj.documentation = data.doc; // TODO: rename documentation -> doc
+        obj.status = data.status.status;
+        obj.times = data.times;
+        return obj
+    }
+
+    function Test(data) {
+        var names = ['name', 'doc', 'status', '...']
+        var test = createModelObject(data);
+        test.fullname = data.parent.fullname + "." + test.name;  // TODO: is this used?, could be function also
+        test.parentName = function () {
+            return data.parent.fullname.replace('.', ' . ', 'g') + ' . '; // TODO: duplicate
+        };
+        test.timeout = data.timeout;
+        test.populateKeywords = createIterablePopulator("Keyword");
+        test.children = function () {
+            return test.keywords();
+        };
+        test.isCritical = data.isCritical;
+        test.statusText = test.status.toUpperCase() + (test.isCritical ? " (critical)" : ""); // TODO: move to templates
+        test.tags = data.tags;
+        test.message = data.message;
+        // TODO: Handle failures in parent teardowns
+        // data.status.parentSuiteTeardownFailed;
+        // return "Teardown of the parent suite failed.";
+        return test;
+    }
+
+    function Keyword(data) {
+        var kw = createModelObject(data);
+        kw.type = data.type;
+        var parent = data.parent
+        var parentPath = (parent.path === undefined) ? parent.fullname : parent.path;
+        kw.path = parentPath + "." + data.index;
+        kw.arguments = data.args;
+        kw.populateKeywords = createIterablePopulator("Keyword");
+        kw.populateMessages = createIterablePopulator("Message");
+        kw.children = function () {
+            return kw.keywords();
+        };
+        return kw;
+    }
+
+    function Message(level, time, text, link) {
+        var message = {};
+        message.level = level;
+        message.levelText = level.toUpperCase();
+        message.time = time;
+        message.shortTime = function () {
+            return timeFromDate(message.time);
+        };
+        message.date = function () {
+            return formatDate(message.time);
+        };
+        message.text = text;
+        message.link = link;
+        return message;
+    }
+
+    function Status(status, parentSuiteTeardownFailed) {
+        return {
+            parentSuiteTeardownFailed: parentSuiteTeardownFailed,
+            status: parentSuiteTeardownFailed ? model.FAIL : status
+        };
+    }
+
+    function Times(timedata) {
+        var start = timedata[0];
+        var end = timedata[1];
+        var elapsed = timedata[2];
+        var times = {}
+        times.elapsedMillis = elapsed;
+        times.elapsedTime = function (excludeMillis) {
+            return formatElapsed(elapsed, excludeMillis);
+        };
+        times.startTime = function (excludeMillis) {
+            return formatDate(start, excludeMillis);
+        }
+        times.endTime = function (excludeMillis) {
+            return formatDate(end, excludeMillis);
+        }
+        return times;
+    }
+
+    function timeFromDate(date) {
+        return shortTime(date.getHours(), date.getMinutes(),
+                date.getSeconds(), date.getMilliseconds());
+    }
+
+    function formatDate(date, excludeMillis) {
+        var milliseconds = date.getMilliseconds();
+        if (excludeMillis)
+            milliseconds = undefined
+        return padTo(date.getFullYear(), 4) +
+                padTo(date.getMonth() + 1, 2) +
+                padTo(date.getDate(), 2) + " " +
+                shortTime(date.getHours(), date.getMinutes(), date.getSeconds(), milliseconds);
+    }
+
+    function shortTime(hours, minutes, seconds, milliseconds) {
+        var ret = padTo(hours, 2) + ":" + padTo(minutes, 2) + ":" + padTo(seconds, 2);
+        if (milliseconds != undefined)
+            ret += "." + padTo(milliseconds, 3);
+        return ret;
+    }
+
+    function formatElapsed(elapsed, excludeMillis) {
+        var millis = elapsed;
+        var hours = Math.floor(millis / (60 * 60 * 1000));
+        millis -= hours * 60 * 60 * 1000;
+        var minutes = Math.floor(millis / (60 * 1000));
+        millis -= minutes * 60 * 1000;
+        var seconds = Math.floor(millis / 1000);
+        millis -= seconds * 1000;
+        if (excludeMillis)
+            millis = undefined;
+        return shortTime(hours, minutes, seconds, millis);
+    }
+
+    function padTo(number, len) {
+        var numString = number + "";
+        while (numString.length < len) numString = "0" + numString;
+        return numString;
+    }
+
+    function createIterablePopulator(name) {
+        return function (populator) {
+            populateIterable(this, name, populator);
+        };
+    }
+
+    function populateIterable(obj, name, populator) {
+        obj["numberOf" + name + "s"] = populator.numberOfItems;
+        var nameInLowerCase = name.toLowerCase();
+        obj[nameInLowerCase] = createGetFunction(populator.numberOfItems, populator.creator);
+        obj[nameInLowerCase + "s"] = createGetAllFunction(populator.numberOfItems, obj[nameInLowerCase]);
+    }
+
+    function createGetFunction(numberOfElements, creator) {
+        var cache = new Array();
+        return function (index) {
+            if (numberOfElements <= index)
+                return undefined;
+            if (!cache[index])
+                cache[index] = creator(index);
+            return cache[index];
+        };
+    }
+
+    function createGetAllFunction(numberOfElements, getter) {
+        var cached = undefined;
+        return function () {
+            if (cached === undefined) {
+                cached = [];
+                for (var i = 0; i < numberOfElements; i++) {
+                    cached.push(getter(i));
+                }
+            }
+            return cached;
+        };
+    }
+
+    return {
+        Suite: Suite,
+        Test: Test,
+        Keyword: Keyword,
+        Message: Message,
+        Status: Status,
+        Times: Times,
+        PASS: STATUS.pass,
+        FAIL: STATUS.fail,
+        formatElapsed: formatElapsed,
+        containsTag: containsTag,  // Exposed for tests
+        shortTime: shortTime
+    };
+}();
+
+window.stats = (function () {
+
+    function Statistics(totalElems, tagElems, suiteElems){
+        return {total: util.map(totalElems, statElem),
+                tag:   util.map(tagElems, tagStatElem),
+                suite: util.map(suiteElems, suiteStatElem)};
+    }
+
+    function statElem(data) {
+        var stat = {
+            label: data[0],
+            pass:  data[1],
+            fail:  data[2],
+            total: data[1] + data[2],
+            doc:   data[3],
+            links: []
+        };
+        var percents = calculatePercents(stat.total, stat.pass, stat.fail);
+        stat.passPercent = percents[0];
+        stat.failPercent = percents[1];
+        var widths = calculateWidths(stat.passPercent, stat.failPercent);
+        stat.passWidth = widths[0];
+        stat.failWidth = widths[1];
+        return stat;
+    }
+
+    function tagStatElem(data) {
+        var stat = statElem(data);
+        stat.info = data[4];
+        if (stat.info)
+            stat.shownInfo = '(' + stat.info + ')';
+        else
+            stat.shownInfo = '';
+        stat.links = parseLinks(data[5]);
+        return stat;
+    }
+
+    function suiteStatElem(data) {
+        var stat = statElem(data);
+        stat.fullname = function () { return stat.doc; };
+        var nameParts = stat.doc.split('.');
+        stat.name = nameParts.pop();
+        if (nameParts)
+            nameParts.push('');
+        stat.parentName = nameParts.join(' . ');
+        return stat;
+    }
+
+    function parseLinks(linksData) {
+        if (!linksData)
+            return [];
+        var items = linksData.split(':::');
+        var links = [];
+        for (var i=0; i<items.length; i++) {
+            parts = items[i].split(':');
+            links[i] = {title: parts[0], url: parts.splice(1).join(':')};
+        }
+        return links;
+    }
+
+    function calculatePercents(total, passed, failed) {
+        if (total == 0)
+            return [0.0, 0.0];
+        pass = 100.0 * passed / total;
+        fail = 100.0 * failed / total;
+        if (pass > 0 && pass < 0.1)
+            return [0.1, 99.9];
+        if (fail > 0 && fail < 0.1)
+            return [99.9, 0.1];
+        return [Math.round(pass*10)/10, Math.round(fail*10)/10];
+    }
+
+    function calculateWidths(num1, num2) {
+        if (num1 + num2 == 0)
+            return [0.0, 0.0];
+        // Make small percentages better visible
+        if (num1 > 0 && num1 < 1)
+            return [1.0, 99.0];
+        if (num2 > 0 && num2 < 1)
+            return [99.0, 1.0];
+        // Handle situation where both are rounded up
+        while (num1 + num2 > 100) {
+            if (num1 > num2)
+                num1 -= 0.1;
+            if (num2 > num1)
+                num2 -= 0.1;
+        }
+        return [num1, num2];
+    }
+
+    return {
+        Statistics: Statistics
+    };
+
+}());
---
+++ utest/serializing/test_js_serializer.py
@@ -0,0 +1,233 @@
+from __future__ import with_statement
+import StringIO
+import json
+import unittest
+import xml.sax as sax
+
+from resources import GOLDEN_OUTPUT, GOLDEN_JS
+from robot.serializing.jsparser import _RobotOutputHandler, Context, parse_js, json_dump
+from robot.utils.asserts import assert_equals
+
+class TestJsSerializer(unittest.TestCase):
+
+    def setUp(self):
+        self._context = Context()
+        self._handler = _RobotOutputHandler(self._context)
+
+    def test_message_xml_parsing(self):
+        data_model = self._get_data_model('<msg timestamp="20110531 12:48:09.088" level="FAIL">AssertionError</msg>')
+        assert_equals(data_model._basemillis, 1306835289088)
+        assert_equals(data_model._robot_data, [0, 'F', 1])
+        assert_equals(data_model._texts, ['*', '*AssertionError'])
+
+    def test_status_xml_parsing(self):
+        data_model = self._get_data_model('<status status="PASS" endtime="20110531 12:48:09.042" starttime="20110531 12:48:09.000"></status>')
+        assert_equals(data_model._basemillis, 1306835289000)
+        assert_equals(data_model._robot_data, ['P',0,42])
+        assert_equals(data_model._texts, ['*'])
+
+    def test_status_with_message_xml_parsing(self):
+        data_model = self._get_data_model('<status status="PASS" endtime="20110531 12:48:09.042" starttime="20110531 12:48:09.000">Message</status>')
+        assert_equals(data_model._basemillis, 1306835289000)
+        assert_equals(data_model._robot_data, ['P',0,42,1])
+        assert_equals(data_model._texts, ['*', '*Message'])
+
+    def test_tags_xml_parsing(self):
+        tags_xml = """
+        <tags>
+            <tag>someothertag</tag>
+            <tag>sometag</tag>
+        </tags>
+        """
+        data_model = self._get_data_model(tags_xml)
+        assert_equals(data_model._robot_data, [1, 2])
+        assert_equals(data_model._texts, ['*', '*someothertag', '*sometag'])
+
+    def test_arguments_xml_parsing(self):
+        arguments_xml = """
+        <arguments>
+            <arg>${arg}</arg>
+            <arg>${level}</arg>
+        </arguments>
+        """
+        data_model = self._get_data_model(arguments_xml)
+        assert_equals(data_model._robot_data, 1)
+        assert_equals(data_model._texts, ['*', '*${arg}, ${level}'])
+
+    def test_keyword_xml_parsing(self):
+        keyword_xml = """
+        <kw type="teardown" name="BuiltIn.Log" timeout="">
+            <doc>Logs the given message with the given level.</doc>
+            <arguments>
+                <arg>keyword teardown</arg>
+            </arguments>
+            <msg timestamp="20110531 12:48:09.070" level="WARN">keyword teardown</msg>
+            <status status="PASS" endtime="20110531 12:48:09.071" starttime="20110531 12:48:09.069"></status>
+        </kw>
+        """
+        self._context.start_suite('suite')
+        data_model = self._get_data_model(keyword_xml)
+        assert_equals(data_model._basemillis, 1306835289070)
+        assert_equals(data_model._robot_data, ['teardown', 1, 0, 2, 3, [0, "W", 3], ["P", -1, 2]])
+        assert_equals(data_model._texts, ['*', '*BuiltIn.Log', '*Logs the given message with the given level.', '*keyword teardown'])
+        assert_equals(self._context.link_to([0, "W", 3]), "keyword_suite.0")
+
+    def test_test_xml_parsing(self):
+        test_xml = """
+        <test name="Test" timeout="">
+            <doc></doc>
+            <kw type="kw" name="Log" timeout="">
+                <doc>Logging</doc>
+                <arguments>
+                    <arg>simple</arg>
+                </arguments>
+                <msg timestamp="20110601 12:01:51.353" level="INFO">simple</msg>
+                <status status="PASS" endtime="20110601 12:01:51.353" starttime="20110601 12:01:51.353"></status>
+            </kw>
+            <tags>
+            </tags>
+            <status status="PASS" endtime="20110601 12:01:51.354" critical="yes" starttime="20110601 12:01:51.353"></status>
+        </test>
+        """
+        self._context.start_suite('SuiteName')
+        data_model = self._get_data_model(test_xml)
+        assert_equals(data_model._basemillis, 1306918911353)
+        assert_equals(data_model._robot_data, ['test', 1, 0, 'Y', 0, ['kw', 2, 0, 3, 4, [0, 'I', 4], ['P', 0, 0]], [], ['P', 0, 1]])
+        assert_equals(data_model._texts, ['*', '*Test', '*Log', '*Logging', '*simple'])
+
+    def test_suite_xml_parsing(self):
+        suite_xml = """<suite source="/tmp/verysimple.txt" name="Verysimple">
+                    <doc></doc>
+                    <metadata></metadata>
+                    <test name="Test" timeout="">
+                        <doc></doc>
+                        <kw type="kw" name="BuiltIn.Log" timeout="">
+                            <doc>Logs the given message with the given level.</doc>
+                            <arguments><arg>simple</arg></arguments>
+                            <msg timestamp="20110601 12:01:51.353" level="WARN">simple</msg>
+                            <status status="PASS" endtime="20110601 12:01:51.353" starttime="20110601 12:01:51.353"></status>
+                        </kw>
+                        <tags></tags>
+                        <status status="PASS" endtime="20110601 12:01:51.354" critical="yes" starttime="20110601 12:01:51.353"></status>
+                    </test>
+                    <status status="PASS" endtime="20110601 12:01:51.354" starttime="20110601 12:01:51.329"></status>
+                    </suite>"""
+        data_model = self._get_data_model(suite_xml)
+        assert_equals(data_model._basemillis, 1306918911353)
+        assert_equals(data_model._robot_data, ['suite', '/tmp/verysimple.txt', 'Verysimple',
+                                               0, {},
+                                                ['test', 1, 0, 'Y', 0,
+                                                    ['kw', 2, 0, 3, 4, [0, 'W', 4], ['P', 0, 0]], [], ['P', 0, 1]],
+                                               ['P', -24, 25], [1, 1, 1, 1]])
+        assert_equals(data_model._texts, ['*', '*Test', '*BuiltIn.Log', '*Logs the given message with the given level.', '*simple'])
+        assert_equals(self._context.link_to([0, 'W', 4]), 'keyword_Verysimple.Test.0')
+
+    def test_metadata_xml_parsing(self):
+        meta_xml = """<metadata>
+                        <item name="meta">&lt;b&gt;escaped&lt;/b&gt;</item>
+                        <item name="version">alpha</item>
+                      </metadata>"""
+        data_model = self._get_data_model(meta_xml)
+        assert_equals(data_model._robot_data, {'meta':1, 'version':2})
+        assert_equals(data_model._texts, ['*', '*<b>escaped</b>', '*alpha'])
+
+    def test_statistics_xml_parsing(self):
+        statistics_xml = """
+        <statistics>
+            <total>
+                <stat fail="4" doc="" pass="0">Critical Tests</stat>
+                <stat fail="4" doc="" pass="0">All Tests</stat>
+            </total>
+            <tag>
+                <stat info="" fail="1" pass="0" links="" doc="">someothertag</stat>
+                <stat info="" fail="1" pass="0" links="" doc="">sometag</stat>
+            </tag>
+            <suite>
+                <stat fail="4" doc="Data" pass="0">Data</stat>
+                <stat fail="1" doc="Data.All Settings" pass="0">Data.All Settings</stat>
+                <stat fail="3" doc="Data.Failing Suite" pass="0">Data.Failing Suite</stat>
+            </suite>
+        </statistics>
+        """
+        data_model = self._get_data_model(statistics_xml)
+        assert_equals(data_model._robot_data, [[['Critical Tests', 0, 4, '', '', ''],
+            ['All Tests', 0, 4, '', '', '']],
+            [['someothertag', 0, 1, '', '', ''],
+                ['sometag', 0, 1, '', '', '']],
+            [['Data', 0, 4, 'Data', '', ''],
+                ['Data.All Settings', 0, 1, 'Data.All Settings', '', ''],
+                ['Data.Failing Suite', 0, 3, 'Data.Failing Suite', '', '']]])
+        assert_equals(data_model._texts, ['*'])
+
+    def test_errors_xml_parsing(self):
+        errors_xml = """
+        <errors>
+            <msg timestamp="20110531 12:48:09.078" level="ERROR">Invalid syntax in file '/tmp/data/failing_suite.txt' in table 'Settings': Resource file 'nope' does not exist.</msg>
+        </errors>
+        """
+        data_model = self._get_data_model(errors_xml)
+        assert_equals(data_model._basemillis, 1306835289078)
+        assert_equals(data_model._robot_data, [[0, 'E', 1]])
+        assert_equals(data_model._texts, ['*', "*Invalid syntax in file '/tmp/data/failing_suite.txt' in table 'Settings': Resource file 'nope' does not exist."])
+
+    def test_json_dump_string(self):
+        from time import time
+        string = u'string\u00A9\v\\\'\"\r\b\t\0\n\fjee'
+        for i in range(1024):
+            string += unichr(i)
+        #string = string * 10
+        buffer = StringIO.StringIO()
+        #s = time()
+        json_dump(string, buffer)
+        #e = time()
+        #print 'json_dump %s' % (e - s)
+        expected = StringIO.StringIO()
+        #s = time()
+        json.dump(string, expected)
+        #e = time()
+        #print 'json.dump %s' % (e - s)
+        self._assert_long_equals(buffer.getvalue(), expected.getvalue())
+
+    def test_json_dump_integer(self):
+        buffer = StringIO.StringIO()
+        json_dump(12, buffer)
+        assert_equals('12', buffer.getvalue())
+
+    def test_json_dump_list(self):
+        buffer = StringIO.StringIO()
+        json_dump([1,2,3, 'hello', 'world'], buffer)
+        assert_equals('[1,2,3,"hello","world"]', buffer.getvalue())
+
+    def test_json_dump_dictionary(self):
+        buffer = StringIO.StringIO()
+        json_dump({'key':1, 'hello':'world'}, buffer)
+        assert_equals('{"hello":"world","key":1}', buffer.getvalue())
+
+    def _get_data_model(self, xml_string):
+        sax.parseString(xml_string, self._handler)
+        return self._handler.datamodel
+
+
+    def test_golden_js_generation(self):
+        buffer = StringIO.StringIO()
+        parse_js(GOLDEN_OUTPUT, buffer)
+        with open(GOLDEN_JS, 'r') as expected:
+            self._assert_long_equals(buffer.getvalue(), expected.read())
+
+    def _assert_long_equals(self, given, expected):
+        if (given!=expected):
+            for index, char in enumerate(given):
+                if index >= len(expected):
+                    raise AssertionError('Expected is shorter than given string. Ending that was missing %s' % given[index:])
+                if char != expected[index]:
+                    raise AssertionError("Given and expected not equal ('%s' != '%s') at index %s.\n'%s' !=\n'%s'" %
+                                         (char, expected[index], index, self._snippet(index, given), self._snippet(index, expected)))
+            if len(expected) > len(given):
+                raise AssertionError('Expected is longer than given string. Ending that was missing %s' % expected[len(given)-1:])
+
+    def _snippet(self, index, string):
+        start = max(0, index-20)
+        start_padding = '' if start==0 else '...'
+        end = min(len(string), index+20)
+        end_padding = '' if end==0 else '...'
+        return start_padding+string[start:end]+end_padding
