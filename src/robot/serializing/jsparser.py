703339e6cab7f6dab0bb7dc4e05b0b7dfe1b33d2
Pekka Kl?rck <Pekka Kl?rck@localhost>
2011-06-10 15:19:04 +0300

    New log/report generation system doesn't support splitting outputs and --splitoutputs option must thus be removed.
    Update issue 861
    Status: Started
    --splitoutputs option is now ignored with a warning.
    
    Todo:
    1) Remove the the code related to splitting
    2) Update user guide

---
+++ src/robot/serializing/jsparser.py
@@ -0,0 +1,525 @@
+from __future__ import with_statement
+import StringIO
+import xml.sax as sax
+from xml.sax.handler import ContentHandler
+import zlib
+import base64
+
+from robot import utils
+
+
+class Context(object):
+
+    def __init__(self):
+        self._texts = TextCache()
+        self._basemillis = None
+        self._stats = Stats()
+        self._current_place = []
+        self._kw_index = []
+        self._links = {}
+
+    @property
+    def basemillis(self):
+        return self._basemillis
+
+    def collect_stats(self):
+        self._stats = self._stats.new_child()
+        return self
+
+    def dump_stats(self):
+        try:
+            return self._stats.dump()
+        finally:
+            self._stats = self._stats.parent
+
+    def get_text_id(self, text):
+        return self._texts.add(text)
+
+    def dump_texts(self):
+        return self._texts.dump()
+
+    def timestamp(self, time):
+        if time == 'N/A':
+            return None
+        millis = int(utils.timestamp_to_secs(time, millis=True) * 1000)
+        if self._basemillis is None:
+            self._basemillis = millis
+        return millis - self.basemillis
+
+    def start_suite(self, name):
+        self._current_place += [('suite', name)]
+        self._kw_index += [0]
+
+    def end_suite(self):
+        self._current_place.pop()
+        self._kw_index.pop()
+
+    def start_test(self, name):
+        self._current_place += [('test', name)]
+        self._kw_index += [0]
+
+    def end_test(self):
+        self._current_place.pop()
+        self._kw_index.pop()
+
+    def start_keyword(self):
+        self._current_place += [('keyword', self._kw_index[-1])]
+        self._kw_index[-1] += 1
+        self._kw_index += [0]
+
+    def end_keyword(self):
+        self._current_place.pop()
+        self._kw_index.pop()
+
+    def create_link_to_current_location(self, key):
+        self._links[str(key)] = self._create_link()
+
+    def _create_link(self):
+        return "keyword_"+".".join(str(v) for _, v in self._current_place)
+
+    def link_to(self, key):
+        return self._links[str(key)]
+
+    def add_test(self, critical, passed):
+        self._stats.add_test(critical, passed)
+
+    def teardown_failed(self):
+        self._stats.fail_all()
+
+
+class Stats(object):
+    TOTAL = 0
+    TOTAL_PASSED = 1
+    CRITICAL = 2
+    CRITICAL_PASSED = 3
+
+    def __init__(self, parent=None):
+        self.parent = parent
+        self._stats = [0,0,0,0]
+        self._children = []
+
+    def new_child(self):
+        self._children += [Stats(self)]
+        return self._children[-1]
+
+    def add_test(self, critical, passed):
+        self._stats[Stats.TOTAL] += 1
+        if passed:
+            self._stats[Stats.TOTAL_PASSED] +=1
+        if critical:
+            self._stats[Stats.CRITICAL] += 1
+            if passed:
+                self._stats[Stats.CRITICAL_PASSED] += 1
+
+    def dump(self):
+        if self.parent:
+            for i in range(4):
+                self.parent._stats[i] += self._stats[i]
+        return self._stats
+
+    def fail_all(self):
+        self._stats[1] = 0
+        self._stats[3] = 0
+        for child in self._children:
+            child.fail_all()
+
+
+class _TextIndex(int):
+    pass
+
+
+class TextCache(object):
+    _zero_index = _TextIndex(0)
+    # TODO: Tune compressing thresholds
+    _compress_threshold = 20
+    _use_compressed_threshold = 1.1
+
+    def __init__(self):
+        self.texts = {}
+        self.index = 1
+
+    def add(self, text):
+        if not text:
+            return self._zero_index
+        text = self._encode(text)
+        if text not in self.texts:
+            self.texts[text] = _TextIndex(self.index)
+            self.index += 1
+        return self.texts[text]
+
+    def _encode(self, text):
+        raw = self._raw(text)
+        if raw in self.texts or len(raw) < self._compress_threshold:
+            return raw
+        compressed = base64.b64encode(zlib.compress(text.encode('UTF-8'), 9))
+        if len(raw) * self._use_compressed_threshold > len(compressed):
+            return compressed
+        return raw
+
+    def _raw(self, text):
+        return '*'+text
+
+    def dump(self):
+        l = range(len(self.texts)+1)
+        l[0] = '*'
+        for k, v in self.texts.items():
+            l[v] = k
+        return l
+
+levels = {'TRACE':'T',
+          'DEBUG':'D',
+          'INFO':'I',
+          'WARN':'W',
+          'ERROR':'E',
+          'FAIL':'F'}
+
+def create_datamodel_from(input_filename):
+    robot = _RobotOutputHandler(Context())
+    with open(input_filename, 'r') as input:
+        sax.parse(input, robot)
+    return robot.datamodel
+
+
+class _Handler(object):
+
+    def __init__(self, context, *args):
+        self._context = context
+        self._children = []
+        self._handlers = {
+        'robot'      : _RobotHandler,
+        'suite'      : _SuiteHandler,
+        'test'       : _TestHandler,
+        'statistics' : _StatisticsHandler,
+        'stat'       : _StatItemHandler,
+        'errors'     : _Handler,
+        'doc'        : _TextHandler,
+        'kw'         : _KeywordHandler,
+        'arg'        : _ArgumentHandler,
+        'arguments'  : _ArgumentsHandler,
+        'tag'        : _TextHandler,
+        'tags'       : _Handler,
+        'msg'        : _MsgHandler,
+        'status'     : _StatusHandler,
+        'metadata'   : _MetadataHandler,
+        'item'       : _MetadataItemHandler,
+    }
+
+    def get_handler_for(self, name, *args):
+        return self._handlers[name](self.context, *args)
+
+    @property
+    def context(self):
+        return self._context
+
+    @property
+    def children(self):
+        return self._children
+
+    def add_child(self, child):
+        self._children += [child]
+
+    def end_element(self, text):
+        return self._children
+
+
+class _RobotHandler(_Handler):
+
+    def __init__(self, context, attrs):
+        _Handler.__init__(self, context, attrs)
+        self._generator = attrs.getValue('generator')
+        self._generated = attrs.getValue('generated')
+
+    def end_element(self, text):
+        if self._generated:
+            self._generated = self.context.timestamp(self._generated)
+        return [self._generated, self._generator] + self.children
+
+
+class _StatisticsHandler(_Handler):
+
+    def get_handler_for(self, name, *args):
+        return _Handler(self.context, *args)
+
+
+class _StatItemHandler(_Handler):
+
+    def __init__(self, context, attrs):
+        _Handler.__init__(self, context, attrs)
+        self._pass = int(attrs.getValue('pass'))
+        self._fail = int(attrs.getValue('fail'))
+        self._doc = attrs.get('doc') or ''
+        self._info = attrs.get('info') or ''
+        self._links = attrs.get('links') or ''
+
+    def end_element(self, text):
+        return [text, self._pass, self._fail, self._doc, self._info, self._links]
+
+
+class _SuiteHandler(_Handler):
+
+    def __init__(self, context, attrs):
+        _Handler.__init__(self, context, attrs)
+        self._name = attrs.getValue('name')
+        self._source = attrs.get('source') or ''
+        self.context.start_suite(self._name)
+        self.context.collect_stats()
+
+    def end_element(self, text):
+        try:
+            return ['suite',
+                     self._source,
+                     self._name] + self._children + [self.context.dump_stats()]
+        finally:
+            self.context.end_suite()
+
+
+class _TestHandler(_Handler):
+
+    def __init__(self, context, attrs):
+        _Handler.__init__(self, context, attrs)
+        name = attrs.getValue('name')
+        self._name_id = self.context.get_text_id(name)
+        self._timeout = self.context.get_text_id(attrs.get('timeout'))
+        self.context.start_test(name)
+
+    def get_handler_for(self, name, *args):
+        if name == 'status':
+            return _TestStatusHandler(self, *args)
+        return _Handler.get_handler_for(self, name, *args)
+
+    def end_element(self, text):
+        result = ['test', self._name_id, self._timeout, self._critical] + self.children
+        self.context.add_test(self._critical == 'Y', result[-1][0] == 'P')
+        self.context.end_test()
+        return result
+
+
+class _StatusHandler(object):
+    def __init__(self, context, attrs):
+        self._context = context
+        self._status = attrs.getValue('status')[0]
+        self._starttime = self._context.timestamp(attrs.getValue('starttime'))
+        endtime = self._context.timestamp(attrs.getValue('endtime'))
+        self._elapsed = endtime-self._starttime if endtime is not None and self._starttime is not None else None
+
+    def end_element(self, text):
+        result = [self._status,
+                  self._starttime,
+                  self._elapsed]
+        if text:
+           result += [self._context.get_text_id(text)]
+        return result
+
+
+class _TestStatusHandler(_StatusHandler):
+
+    def __init__(self, test, attrs):
+        _StatusHandler.__init__(self, test.context, attrs)
+        test._critical = 'Y' if attrs.get('critical') == 'yes' else 'N'
+
+
+class _KeywordHandler(_Handler):
+
+    def __init__(self, context, attrs):
+        _Handler.__init__(self, context, attrs)
+        self.context.start_keyword()
+        self._type = attrs.getValue('type')
+        self._name = self.context.get_text_id(attrs.getValue('name'))
+        self._timeout = self.context.get_text_id(attrs.getValue('timeout'))
+
+    def end_element(self, text):
+        if self._type == 'teardown' and self.children[-1][0] == 'F':
+            self.context.teardown_failed()
+        self.context.end_keyword()
+        return [self._type, self._name, self._timeout]+self.children
+
+
+class _ArgumentHandler(_Handler):
+
+    def end_element(self, text):
+        return text
+
+
+class _ArgumentsHandler(_Handler):
+
+    def end_element(self, text):
+        return self._context.get_text_id(', '.join(self.children))
+
+
+class _TextHandler(_Handler):
+
+    def end_element(self, text):
+        return self.context.get_text_id(text)
+
+
+class _MetadataHandler(_Handler):
+
+    def __init__(self, context, attrs):
+        _Handler.__init__(self, context, attrs)
+        self._dictionary = {}
+
+    def add_child(self, child):
+        self._dictionary[child[0]] = child[1]
+
+    def end_element(self, text):
+        return self._dictionary
+
+
+class _MetadataItemHandler(_Handler):
+
+    def __init__(self, context, attrs):
+        _Handler.__init__(self, context, attrs)
+        self._name = attrs.getValue('name')
+
+    def end_element(self, text):
+        return (self._name, self.context.get_text_id(text))
+
+
+class _MsgHandler(object):
+
+    def __init__(self, context, attrs):
+        self._context = context
+        self._msg = []
+        self._msg += [self._context.timestamp(attrs.getValue('timestamp'))]
+        self._msg += [levels[attrs.getValue('level')]]
+        self._is_html = attrs.get('html')
+        self._is_linkable = attrs.get("linkable") == "yes"
+
+    def end_element(self, text):
+        self._add_text(text)
+        self._handle_warning_linking()
+        return self._msg
+
+    def _handle_warning_linking(self):
+        if self._is_linkable:
+            self._msg += [self._context.link_to(self._msg)]
+        elif self._msg[1] == 'W':
+            self._context.create_link_to_current_location(self._msg)
+
+    def _add_text(self, text):
+        if self._is_html:
+            self._msg += [self._context.get_text_id(text)]
+        else:
+            self._msg += [self._context.get_text_id(utils.html_escape(text, replace_whitespace=False))]
+
+
+class _RootHandler(_Handler):
+
+    def add_child(self, child):
+        self.data = child
+
+
+class _RobotOutputHandler(ContentHandler):
+
+    def __init__(self, context):
+        self._context = context
+        self._handler_stack = [_RootHandler(context)]
+
+    @property
+    def datamodel(self):
+        return DataModel(self._context.basemillis, self._handler_stack[0].data, self._context.dump_texts())
+
+    def startElement(self, name, attrs):
+        handler = self._handler_stack[-1].get_handler_for(name, attrs)
+        self._charbuffer = StringIO.StringIO()
+        self._handler_stack.append(handler)
+
+    def endElement(self, name):
+        handler = self._handler_stack.pop()
+        self._handler_stack[-1].add_child(handler.end_element(self._charbuffer.getvalue()))
+
+    def characters(self, content):
+        self._charbuffer.write(content)
+
+
+class DataModel(object):
+
+    def __init__(self, basemillis, robot_data, texts):
+        self._basemillis = basemillis
+        self._robot_data = robot_data
+        self._texts = texts
+
+    def write_to(self, output):
+        output.write('window.basemillis = '+str(self._basemillis)+';\n')
+        output.write('window.data = ')
+        json_dump(self._robot_data, output)
+        output.write(';\n')
+        output.write('window.strings =')
+        json_dump(self._texts, output)
+        output.write(';\n')
+
+    def remove_keywords(self):
+        self._robot_data = self._remove_keywords_from(self._robot_data)
+        self._prune_unused_texts()
+
+    def _remove_keywords_from(self, data):
+        if not isinstance(data, list):
+            return data
+        return [self._remove_keywords_from(item) for item in data if not self._is_keyword(item)]
+
+    def _is_keyword(self, item):
+        return isinstance(item, list) and item and item[0] in ['kw', 'setup', 'teardown']
+
+    def _prune_unused_texts(self):
+        used = self._collect_used_text_indices(self._robot_data, set())
+        self._texts = [text if index in used else '' for index, text in enumerate(self._texts)]
+
+    def _collect_used_text_indices(self, data, result):
+        for item in data:
+            if isinstance(item, _TextIndex):
+                result.add(item)
+            elif isinstance(item, list):
+                self._collect_used_text_indices(item, result)
+            elif isinstance(item, dict):
+                self._collect_used_text_indices(item.values(), result)
+        return result
+
+
+def encode_basestring(string):
+    def get_matching_char(c):
+        val = ord(c)
+        if val < 127 and val > 31:
+            return c
+        return '\\u' + hex(val)[2:].rjust(4,'0')
+    string = string.replace('\\', '\\\\')
+    string = string.replace('"', '\\"')
+    string = string.replace('\b', '\\b')
+    string = string.replace('\f', '\\f')
+    string = string.replace('\n', '\\n')
+    string = string.replace('\r', '\\r')
+    string = string.replace('\t', '\\t')
+    result = StringIO.StringIO()
+    for c in string:
+        result.write(get_matching_char(c))
+    return '"'+result.getvalue()+'"'
+
+def json_dump(data, output):
+    if data is None:
+        output.write('null')
+    elif isinstance(data, int):
+        output.write(str(data))
+    elif isinstance(data, long):
+        output.write(str(data))
+    elif isinstance(data, basestring):
+        output.write(encode_basestring(data))
+    elif isinstance(data, list):
+        output.write('[')
+        for index, item in enumerate(data):
+            json_dump(item, output)
+            if index < len(data)-1:
+                output.write(',')
+        output.write(']')
+    elif type(data) == dict:
+        output.write('{')
+        for index, item in enumerate(data.items()):
+            json_dump(item[0], output)
+            output.write(':')
+            json_dump(item[1], output)
+            if index < len(data)-1:
+                output.write(',')
+        output.write('}')
+    else:
+        raise Exception('Data type (%s) serialization not supported' % type(data))
+
+def parse_js(input_filename, output):
+    create_datamodel_from(input_filename).write_to(output)
---
+++ src/robot/serializing/serialize_log.py
@@ -0,0 +1,140 @@
+from __future__ import with_statement
+
+import re
+import os
+import robot
+from robot import utils
+
+PATH = os.path.join(os.path.dirname(robot.__file__),'webcontent')
+LOG_TEMPLATE = os.path.join(PATH,'log.html')
+REPORT_TEMPLATE = os.path.join(PATH, 'report.html')
+JS_FILE_REGEXP = re.compile('src=\"([^\"]+)\"')
+
+def serialize_log(test_output_datamodel, log_path, title=None):
+    if log_path is None:
+        return
+    _build_file(log_path, test_output_datamodel, title, None, LOG_TEMPLATE)
+
+def serialize_report(test_output_datamodel, report_path, title=None, background=None, log_path=None):
+    if report_path is None:
+        return
+    relative_log_path = _build_relative_log_path(report_path, log_path)
+    _build_file(report_path, test_output_datamodel, title, _resolve_background_colors(background), REPORT_TEMPLATE, relative_log_path)
+
+def _build_relative_log_path(report, log):
+    if not log:
+        return None
+    return utils.get_link_path(log, os.path.dirname(report))
+
+def _build_file(outpath, test_output_datamodel, title, background, template, log_path=None):
+    with open(outpath, 'w') as outfile:
+        populator = _Populator(outfile, test_output_datamodel, title, background, log_path)
+        with open(template, 'r') as templ:
+            for line in templ:
+                populator.line(line)
+
+def _resolve_background_colors(color_str):
+    if color_str and color_str.count(':') not in [1, 2]:
+        #LOGGER.error("Invalid background color '%s'." % color_str)
+        color_str = None
+    if not color_str:
+        color_str = '#99FF66:#FF3333'
+    colors = color_str.split(':', 2)
+    return colors if len(colors) == 3 else [colors[0], colors[0], colors[1]]
+
+
+class _Populator(object):
+
+    def __init__(self, log, test_output_datamodel, title, background, log_path=None):
+        self._log = log
+        self._log_path=log_path
+        self._test_output_datamodel = test_output_datamodel
+        self._title = title
+        self._parsing = self._normal_parsing
+        self._backgrounds = {"/* BACKGROUND: critical pass */":     background[0] if background else None,
+                             "/* BACKGROUND: non critical fail */": background[1] if background else None,
+                             "/* BACKGROUND: critical fail */":     background[2] if background else None}
+
+    def line(self, line):
+        self._parsing(line)
+
+    def _normal_parsing(self, line):
+        if self._is_begin_scripts(line):
+            self._start_script()
+        elif self._is_title_line_to_handle(line):
+            self._write_title()
+        elif self._is_background_line_to_handle(line):
+            self._write_background(line)
+        elif self._is_log_path_line_to_handle(line):
+            self._replace_log_path(line)
+        else:
+            self._log.write(line)
+
+    def _is_begin_scripts(self, line):
+        return line == '<!-- BEGIN SCRIPTS -->\n'
+
+    def _is_title_line_to_handle(self, line):
+        return self._title is not None and line.startswith('<title>')
+
+    def _write_title(self):
+        self._log.write('<title>%s</title>\n' % self._title)
+
+    def _is_log_path_line_to_handle(self, line):
+        return self._log_path and 'log.html' in line
+
+    def _replace_log_path(self, line):
+        self._log.write(line.replace('log.html', self._log_path))
+
+    def _is_background_line_to_handle(self, line):
+        for marker in self._backgrounds:
+            if marker in line:
+                return True
+        return False
+
+    def _write_background(self, line):
+        for marker in self._backgrounds:
+            if marker in line:
+                self._log.write("    background: %s;\n" % self._backgrounds[marker])
+
+    def _in_script(self, line):
+        if self._is_end_scripts(line):
+            self._end_script()
+        elif self._is_output_js(line):
+            self._write_output_js()
+        else:
+            self._write_lines_js(line)
+
+    def _is_end_scripts(self, line):
+        return line == '<!-- END SCRIPTS -->\n'
+
+    def _end_script(self):
+        self._log.write('</script>\n')
+        self._parsing = self._normal_parsing
+
+    def _is_output_js(self, line):
+        return line.startswith('<!-- OUTPUT JS -->')
+
+    def _write_output_js(self):
+        self._test_output_datamodel.write_to(self._log)
+
+    def _write_lines_js(self, line):
+        self._write_js(self._parse_js_file_name(line))
+
+    def _start_script(self):
+        self._log.write('<script type="text/javascript">\n')
+        self._parsing = self._in_script
+
+    def _parse_js_file_name(self, line):
+        return os.path.join(PATH, JS_FILE_REGEXP.search(line).group(1).replace('/', os.path.sep))
+
+    def _write_js(self, js_file):
+        with open(js_file, 'r') as js:
+            for jsline in js:
+                self._log.write(jsline)
+        self._log.write('\n\n')
+
+
+if __name__ == '__main__':
+    import jsparser
+    jsparser.parse('output.xml', 'output.js')
+    serialize_log('output.js', 'logjsx.html')
