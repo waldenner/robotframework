4fcb1e5bbf773f83b59a3591e673e01ba5f16084
Mikko Korpela <mikko.korpela@gmail.com>
2011-06-13 14:56:05 +0300

    rename no status tests to status tests

---
+++ utest/output/test_filelogger.py
@@ -0,0 +1,50 @@
+import unittest
+import os
+from StringIO import StringIO
+
+from robot import utils
+from robot.utils.asserts import *
+
+from robot.output.filelogger import FileLogger
+
+
+class TestFileLogger(unittest.TestCase):
+    
+    def setUp(self):
+        FileLogger._get_writer = lambda *args: StringIO()
+        self.logger = FileLogger('whatever', 'INFO')
+        utils.robottime._CURRENT_TIME = (2006, 6, 13, 8, 37, 42, 123)
+   
+    def tearDown(self):
+        utils.robottime._CURRENT_TIME = None
+
+    def test_write(self):
+        self.logger.write('my message', 'INFO')
+        expected = '20060613 08:37:42.123 | INFO  | my message\n'
+        self._verify_message(expected)
+        self.logger.write('my 2nd msg\nwith 2 lines', 'ERROR')
+        expected += '20060613 08:37:42.123 | ERROR | my 2nd msg\nwith 2 lines\n'
+        self._verify_message(expected)
+                        
+    def test_write_helpers(self):
+        self.logger.info('my message')
+        expected = '20060613 08:37:42.123 | INFO  | my message\n'
+        self._verify_message(expected)
+        self.logger.warn('my 2nd msg\nwith 2 lines')
+        expected += '20060613 08:37:42.123 | WARN  | my 2nd msg\nwith 2 lines\n'
+        self._verify_message(expected)
+
+    def test_set_level(self):
+        self.logger.write('msg', 'DEBUG')
+        self._verify_message('')
+        self.logger.set_level('DEBUG')
+        self.logger.write('msg', 'DEBUG')
+        self._verify_message('20060613 08:37:42.123 | DEBUG | msg\n')
+
+    def _verify_message(self, expected):
+        expected = expected.replace('\n', os.linesep)
+        assert_equals(self.logger._writer.getvalue(), expected)
+
+if __name__ == "__main__":
+    unittest.main()
+
