10a746731462c7d0a7dafce02b631fab2c90e7ce
Janne H?rk?nen <jth@reaktor.fi>
2011-06-14 10:52:22 +0300

    last tests that do static html check

---
+++ atest/testdata/test_libraries/DeprecatedKeywords.py
@@ -0,0 +1,22 @@
+class DeprecatedKeywords:
+    
+    def deprecated_library_keyword(self):
+        """*DEPRECATED* Use keyword `Not Deprecated With Doc` instead!
+        
+        Some more doc here. ignore this in the warning.
+        """
+        pass
+
+    def deprecated_keyword_returning(self):
+        """*DEPRECATED* But still returning a value!"""
+        return 42
+    
+    def not_deprecated_with_doc(self):
+        """Some Short Doc
+        
+        Some more doc and ignore this *DEPRECATED*
+        """
+        pass
+    
+    def not_deprecated_without_doc(self):
+        pass
