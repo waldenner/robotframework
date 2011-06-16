fbba65d886901b66b139c349487166e370d49e05
mikko korpela <mikko.korpela@gmail.com>
2011-06-16 17:53:46 +0000

    jsxcompressor download date as there is no revision number

---
+++ src/robot/common/statistics.py
@@ -0,0 +1,301 @@
+#  Copyright 2008-2011 Nokia Siemens Networks Oyj
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import re
+
+from robot import utils
+
+
+class Statistics:
+
+    def __init__(self, suite, suite_stat_level=-1, tag_stat_include=None,
+                 tag_stat_exclude=None, tag_stat_combine=None, tag_doc=None,
+                 tag_stat_link=None):
+        self.tags = TagStatistics(tag_stat_include, tag_stat_exclude,
+                                  tag_stat_combine, tag_doc, tag_stat_link)
+        self.suite = SuiteStatistics(suite, self.tags, suite_stat_level)
+        self.total = TotalStatistics(self.suite)
+        self.tags.sort()
+
+    def serialize(self, serializer):
+        serializer.start_statistics(self)
+        self.total.serialize(serializer)
+        self.tags.serialize(serializer)
+        self.suite.serialize(serializer)
+        serializer.end_statistics(self)
+
+
+class Stat:
+
+    def __init__(self, name=''):
+        self.name = name
+        self.passed = 0
+        self.failed = 0
+
+    def add_stat(self, other):
+        self.passed += other.passed
+        self.failed += other.failed
+
+    def add_test(self, test):
+        if test.status == 'PASS':
+            self.passed += 1
+        else:
+            self.failed += 1
+
+    def fail_all(self):
+        self.failed += self.passed
+        self.passed = 0
+
+    def __cmp__(self, other):
+        return cmp(self.name, other.name)
+
+    def __nonzero__(self):
+        return self.failed == 0
+
+
+class SuiteStat(Stat):
+    type = 'suite'
+
+    def __init__(self, suite):
+        Stat.__init__(self, suite.name)
+        self.long_name = suite.get_long_name()
+
+    def serialize(self, serializer):
+        serializer.suite_stat(self)
+
+
+class TagStat(Stat):
+    type = 'tag'
+
+    def __init__(self, name, critical=False, non_critical=False, info=None,
+                 combined=''):
+        Stat.__init__(self, name)
+        self.doc = info.get_doc(name) if info else ''
+        self.critical = critical
+        self.non_critical = non_critical
+        self.tests = []
+        self.links = info.get_links(name) if info else []
+        self.combined = combined
+
+    def add_test(self, test):
+        Stat.add_test(self, test)
+        self.tests.append(test)
+
+    def __cmp__(self, other):
+        if self.critical != other.critical:
+            return cmp(other.critical, self.critical)
+        if self.non_critical != other.non_critical:
+            return cmp(other.non_critical, self.non_critical)
+        if bool(self.combined) != bool(other.combined):
+            return cmp(bool(other.combined), bool(self.combined))
+        return cmp(self.name, other.name)
+
+    def serialize(self, serializer):
+        serializer.tag_stat(self)
+
+
+class TotalStat(Stat):
+    type = 'total'
+
+    def __init__(self, name, suite_stat):
+        Stat.__init__(self, name)
+        self.passed = suite_stat.passed
+        self.failed = suite_stat.failed
+
+    def serialize(self, serializer):
+        serializer.total_stat(self)
+
+
+class SuiteStatistics:
+
+    def __init__(self, suite, tag_stats, suite_stat_level=-1):
+        self.all = SuiteStat(suite)
+        self.critical = SuiteStat(suite)
+        self.suites = []
+        self._process_suites(suite, tag_stats)
+        self._process_tests(suite, tag_stats)
+        self._suite_stat_level = suite_stat_level
+
+    def _process_suites(self, suite, tag_stats):
+        for subsuite in suite.suites:
+            substat = SuiteStatistics(subsuite, tag_stats)
+            self.suites.append(substat)
+            self.all.add_stat(substat.all)
+            self.critical.add_stat(substat.critical)
+
+    def _process_tests(self, suite, tag_stats):
+        for test in suite.tests:
+            self.all.add_test(test)
+            if test.critical == 'yes':
+                self.critical.add_test(test)
+            tag_stats.add_test(test, suite.critical)
+
+    def serialize(self, serializer):
+        if self._suite_stat_level == 0:
+            return
+        serializer.start_suite_stats(self)
+        self._serialize(serializer, self._suite_stat_level)
+        serializer.end_suite_stats(self)
+
+    def _serialize(self, serializer, max_suite_level, suite_level=1):
+        self.all.serialize(serializer)
+        if max_suite_level < 0 or max_suite_level > suite_level:
+            for suite in self.suites:
+                suite._serialize(serializer, max_suite_level, suite_level+1)
+
+
+class TagStatistics:
+
+    def __init__(self, include=None, exclude=None, tag_stat_combine=None,
+                 docs=None, links=None):
+        self.stats = utils.NormalizedDict()
+        self._include = include or []
+        self._exclude = exclude or []
+        self._patterns_and_names = self._get_patterns_and_names(tag_stat_combine)
+        self._taginfo = TagStatInfo(docs or [], links or [])
+
+    def _get_patterns_and_names(self, tag_stat_combine_options):
+        if not tag_stat_combine_options:
+            return []
+        return [ self._parse_name_and_pattern_from(option) \
+                 for option in tag_stat_combine_options ]
+
+    def _parse_name_and_pattern_from(self, option):
+        pattern = option.replace('&', ' & ').replace('NOT', ' NOT ')
+        if ':' in pattern:
+            return pattern.rsplit(':', 1)
+        return pattern, pattern
+
+    def add_test(self, test, critical):
+        self._add_tags_statistics(test, critical)
+        self._add_tagstatcombine_statistics(test)
+
+    def _add_tags_statistics(self, test, critical):
+        for tag in test.tags:
+            if not self._is_included(tag):
+                continue
+            if tag not in self.stats:
+                self.stats[tag] = TagStat(tag, critical.is_critical(tag),
+                                          critical.is_non_critical(tag),
+                                          self._taginfo)
+            self.stats[tag].add_test(test)
+
+    def _is_included(self, tag):
+        if self._include != [] and not utils.matches_any(tag, self._include):
+            return False
+        return not utils.matches_any(tag, self._exclude)
+
+    def _add_tagstatcombine_statistics(self, test):
+        for pattern, name in self._patterns_and_names:
+            if name not in self.stats:
+                self.stats[name] = TagStat(name, combined=pattern)
+            if test.is_included([pattern], []):
+                self.stats[name].add_test(test)
+
+    def serialize(self, serializer):
+        if not self.stats and (self._include or self._exclude):
+            return
+        serializer.start_tag_stats(self)
+        stats = self.stats.values()
+        stats.sort()
+        for stat in stats:
+            stat.serialize(serializer)
+        serializer.end_tag_stats(self)
+
+    def sort(self):
+        for stat in self.stats.values():
+            stat.tests.sort()
+
+
+class TotalStatistics:
+
+    def __init__(self, suite):
+        self.critical = TotalStat('Critical Tests', suite.critical)
+        self.all = TotalStat('All Tests', suite.all)
+
+    def serialize(self, serializer):
+        serializer.start_total_stats(self)
+        self.critical.serialize(serializer)
+        self.all.serialize(serializer)
+        serializer.end_total_stats(self)
+
+
+class TagStatInfo:
+
+    def __init__(self, docs, links):
+        self._docs = [ self._parse_doc(doc) for doc in docs ]
+        self._links = [ TagStatLink(*link) for link in links ]
+
+    def _parse_doc(self, cli_item):
+        try:
+            tag, doc = cli_item.split(':', 1)
+        except ValueError:
+            tag, doc = cli_item, ''
+        return tag, doc
+
+    def get_doc(self, tag):
+        docs = []
+        for pattern, doc in self._docs:
+            if utils.matches(tag, pattern):
+                docs.append(doc)
+        return ' '.join(docs) if docs else ''
+
+    def get_links(self, tag):
+        links = [ link.get_link(tag) for link in self._links ]
+        return [ link for link in links if link is not None ]
+
+
+class TagStatLink:
+    _match_pattern_tokenizer = re.compile('(\*|\?)')
+
+    def __init__(self, pattern, link, title):
+        self._regexp = self._get_match_regexp(pattern)
+        self._link = link
+        self._title = title.replace('_', ' ')
+
+    def get_link(self, tag):
+        match = self._regexp.match(tag)
+        if match is not None:
+            link = self._replace_matches(self._link, match)
+            return link, self._title
+        return None
+
+    def _replace_matches(self, url, match):
+        groups = match.groups()
+        for i, group in enumerate(groups):
+            url = url.replace('%%%d' % (i+1), group)
+        return url
+
+    def _get_match_regexp(self, pattern):
+        regexp = []
+        open_parenthesis = False
+        for token in self._match_pattern_tokenizer.split(pattern):
+            if token == '':
+                continue
+            if token == '?':
+                if not open_parenthesis:
+                    regexp.append('(')
+                    open_parenthesis = True
+                regexp.append('.')
+                continue
+            if open_parenthesis:
+                regexp.append(')')
+                open_parenthesis = False
+            if token == '*':
+                regexp.append('(.*)')
+                continue
+            regexp.append(re.escape(token))
+        if open_parenthesis:
+            regexp.append(')')
+        return re.compile('^%s$' % ''.join(regexp), re.IGNORECASE)
---
+++ src/robot/webcontent/testdata.js
@@ -0,0 +1,337 @@
+window.testdata = function () {
+
+    var elementsById = {};
+    var LEVEL = {I:'info', H:'info', T:'trace', W:'warn', E:'error', D:'debug', F:'fail'};
+    var KEYWORD_TYPE = {kw: 'KEYWORD',
+        setup:'SETUP',
+        teardown:'TEARDOWN'};
+    var _statistics = null;
+
+    function addElement(elem) {
+        elem.id = uuid();
+        elementsById[elem.id] = elem;
+        return elem;
+    }
+
+    function uuid() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+
+    function timestamp(millis) {
+        return new Date(window.output.baseMillis + millis);
+    }
+
+    // TODO: Remove this function and use texts.get everywhere.
+    function get(id) {
+        return texts.get(id)
+    }
+
+    function times(stats) {
+        var startMillis = stats[1];
+        var elapsed = stats[2];
+        if(startMillis == null){
+            return [null, null, elapsed];
+        }
+        return [timestamp(startMillis), timestamp(startMillis + elapsed), elapsed];
+    }
+
+    function message(element) {
+        return addElement(
+                model.Message(LEVEL[element[1]], timestamp(element[0]), get(element[2]), element[3]));
+    }
+
+    // TODO: Is separate status object needed? Probably not.
+    function createStatus(stats, parentSuiteTeardownFailed) {
+        var status = {'P': model.PASS , 'F': model.FAIL, 'N': model.NOT_RUN}[stats[0]];
+        return model.Status(status, parentSuiteTeardownFailed);
+    }
+
+    function last(items) {
+        return items[items.length-1];
+    }
+
+    // TODO: Consider better name...
+    function last2(items) {
+        return items[items.length-2];
+    }
+
+    function childCreator(parent, childType) {
+        return function (elem, index) {
+            return addElement(childType(parent, elem, index));
+        };
+    }
+
+    function createKeyword(parent, element, index) {
+        var kw = model.Keyword({
+            type: KEYWORD_TYPE[element[0]],
+            name: get(element[1]),
+            args: get(element[4]),
+            doc: get(element[3]),
+            status: createStatus(last(element)),
+            times: model.Times(times(last(element))),
+            parent: parent,
+            index: index
+        });
+        kw.populateKeywords(Populator(element, keywordMatcher, childCreator(kw, createKeyword)));
+        kw.populateMessages(Populator(element, messageMatcher, message));
+        return kw;
+    }
+
+    var keywordMatcher = headerMatcher("kw", "setup", "teardown");
+
+    function messageMatcher(elem) {
+        return (elem.length == 3 &&
+                typeof(elem[0]) == "number" &&
+                typeof(elem[1]) == "string" &&
+                typeof(elem[2]) == "number");
+    }
+
+    function tags(taglist) {
+        return util.map(taglist, texts.get);
+    }
+
+    function createTest(suite, element) {
+        var statusElement = last(element);
+        var test = model.Test({
+            parent: suite,
+            name: get(element[1]),
+            doc: get(element[4]),
+            timeout: get(element[2]),
+            isCritical: (element[3] == "Y"),
+            status: createStatus(statusElement, suite.hasTeardownFailure()),
+            message: createMessage(statusElement, suite.hasTeardownFailure()),
+            times: model.Times(times(statusElement)),
+            tags: tags(last2(element))
+        });
+        test.populateKeywords(Populator(element, keywordMatcher, childCreator(test, createKeyword)));
+        return test;
+    }
+
+    function createMessage(statusElement, hasSuiteTeardownFailed) {
+        var message = '';
+        if (statusElement.length == 4)
+            message = get(statusElement[3]);
+        if(hasSuiteTeardownFailed)
+            if(message === '')
+                return 'Teardown of the parent suite failed.';
+            else
+                message += '\n\nAlso teardown of the parent suite failed.'
+        return message;
+    }
+
+    function createSuite(parent, element) {
+        var statusElement = last2(element);
+        var suite = model.Suite({
+            parent: parent,
+            name: element[2],
+            source: element[1],
+            doc: get(element[3]),
+            status: createStatus(statusElement, parent && parent.hasTeardownFailure()),
+            message: createMessage(statusElement, parent && parent.hasTeardownFailure()),
+            times: model.Times(times(statusElement)),
+            statistics: suiteStats(last(element)),
+            metadata: parseMetadata(element[4])
+        });
+        suite.populateKeywords(Populator(element, keywordMatcher, childCreator(suite, createKeyword)));
+        suite.populateTests(Populator(element, headerMatcher("test"), childCreator(suite, createTest)));
+        suite.populateSuites(Populator(element, headerMatcher("suite"), childCreator(suite, createSuite)));
+        return suite;
+    }
+
+    function parseMetadata(data) {
+        var metadata = {};
+        for (var key in data) {
+            metadata[key] = get(data[key]);
+        }
+        return metadata;
+    }
+
+    function suiteStats(stats) {
+        return {
+            total: stats[0],
+            totalPassed: stats[1],
+            totalFailed: stats[0] - stats[1],
+            critical: stats[2],
+            criticalPassed: stats[3],
+            criticalFailed: stats[2] - stats[3]
+        };
+    }
+
+    function headerMatcher() {
+        var args = arguments;
+        return function(elem) {
+            for (var i = 0; i < args.length; i++)
+                if (elem[0] == args[i]) return true;
+            return false;
+        };
+    }
+
+    function Populator(element, matcher, creator) {
+        var items = findElements(element, matcher);
+        return {
+            numberOfItems: items.length,
+            creator: function (index) {
+                return creator(items[index], index);
+            }
+        };
+    }
+
+    function findElements(fromElement, matcher) {
+        var results = new Array();
+        for (var i = 0; i < fromElement.length; i++)
+            if (matcher(fromElement[i]))
+                results.push(fromElement[i]);
+        return results;
+    }
+
+    function suite() {
+        var elem = window.output.suite;
+        if (elementsById[elem.id])
+            return elem;
+        var main = addElement(createSuite(undefined, elem));
+        window.output.suite = main;
+        return main;
+    }
+
+    function findById(id) {
+        return elementsById[id];
+    }
+
+    function pathToKeyword(fullname) {
+        var root = suite();
+        if (fullname.indexOf(root.fullname + ".") != 0) return [];
+        return keywordPathTo(fullname + ".", root, [root.id]);
+    }
+
+    function pathToTest(fullname) {
+        var root = suite();
+        if (fullname.indexOf(root.fullname + ".") != 0) return [];
+        return testPathTo(fullname, root, [root.id]);
+    }
+
+    function pathToSuite(fullname) {
+        var root = suite();
+        if (fullname.indexOf(root.fullname) != 0) return [];
+        if (fullname == root.fullname) return [root.id];
+        return suitePathTo(fullname, root, [root.id]);
+    }
+
+    function keywordPathTo(fullname, current, result) {
+        if (fullname == "") return result;
+        for (var i = 0; i < current.numberOfKeywords; i++) {
+            var kw = current.keyword(i);
+            if (fullname.indexOf(kw.path + ".") == 0) {
+                result.push(kw.id);
+                if (fullname == kw.path + ".")
+                    return result;
+                return keywordPathTo(fullname, kw, result);
+            }
+        }
+        for (var i = 0; i < current.numberOfTests; i++) {
+            var test = current.test(i);
+            if (fullname.indexOf(test.fullname + ".") == 0) {
+                result.push(test.id);
+                return keywordPathTo(fullname, test, result);
+            }
+        }
+        for (var i = 0; i < current.numberOfSuites; i++) {
+            var suite = current.suite(i);
+            if (fullname.indexOf(suite.fullname + ".") == 0) {
+                result.push(suite.id);
+                return keywordPathTo(fullname, suite, result);
+            }
+        }
+    }
+
+    function testPathTo(fullname, currentSuite, result) {
+        for (var i = 0; i < currentSuite.numberOfTests; i++) {
+            var test = currentSuite.test(i);
+            if (fullname == test.fullname) {
+                result.push(test.id);
+                return result;
+            }
+        }
+        for (var i = 0; i < currentSuite.numberOfSuites; i++) {
+            var suite = currentSuite.suite(i);
+            if (fullname.indexOf(suite.fullname + ".") == 0) {
+                result.push(suite.id);
+                return testPathTo(fullname, suite, result);
+            }
+        }
+    }
+
+    function suitePathTo(fullname, currentSuite, result) {
+        for (var i = 0; i < currentSuite.numberOfSuites; i++) {
+            var suite = currentSuite.suite(i);
+            if (fullname == suite.fullname) {
+                result.push(suite.id);
+                return result;
+            }
+            if (fullname.indexOf(suite.fullname + ".") == 0) {
+                result.push(suite.id);
+                return suitePathTo(fullname, suite, result);
+            }
+        }
+    }
+
+    function generated() {
+        return timestamp(window.output.generatedMillis);
+    }
+
+    function errors() {
+        return util.map(window.output.errors, message);
+    }
+
+    // TODO: Is this used anymore?
+    function error(index) {
+        return errors()[index];
+    }
+
+    function statistics() {
+        if (!_statistics) {
+            var statData = window.output.stats;
+            _statistics = stats.Statistics(statData[0], statData[1], statData[2]);
+        }
+        return _statistics
+    }
+
+    return {
+        suite: suite,
+        errors: errors,
+        error: error,
+        find: findById,
+        pathToTest: pathToTest,
+        pathToSuite: pathToSuite,
+        pathToKeyword: pathToKeyword,
+        generated: generated,
+        statistics: statistics
+    };
+
+}();
+
+window.texts = (function () {
+
+    function decodeAndReplace(id) {
+        var text = window.output.strings[id];
+        if (text[0] == '*') {
+            return text.substring(1)
+        }
+        var extracted = extract(text);
+        window.output.strings[id] = "*"+extracted;
+        return extracted;
+    }
+
+    function extract(text) {
+        var decoded = JXG.Util.Base64.decodeAsArray(text);
+        var extracted = (new JXG.Util.Unzip(decoded)).unzip()[0][0];
+        return JXG.Util.utf8Decode(extracted);
+    }
+
+    return {
+        get: function (id) { return decodeAndReplace(id); }
+    };
+
+})();
