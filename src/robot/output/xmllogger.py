306f52797bc4b1d47da55279c9d43baa4dc5f86e
Pekka Kl?rck <Pekka Kl?rck@localhost>
2011-06-23 10:20:26 +0300

    Tuned texts and styles for JS disabled error box. At some point need to test that with older browsers.

---
+++ src/robot/output/xmllogger.py
@@ -0,0 +1,189 @@
+#  Copyright 2008-2011 Nokia Siemens Networks Oyj
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from robot import utils
+from robot.errors import DataError
+from robot.version import get_full_version
+
+from loggerhelper import IsLogged
+
+
+class XmlLogger:
+
+    def __init__(self, path, log_level='TRACE', generator='Robot'):
+        self._log_message_is_logged = IsLogged(log_level)
+        self._error_message_is_logged = IsLogged('WARN')
+        self._writer = None
+        self._writer_args = (path, {'generator': get_full_version(generator),
+                                    'generated': utils.get_timestamp()})
+        self._errors = []
+
+    def _get_writer(self, path, attrs={}):
+        try:
+            writer = utils.XmlWriter(path)
+        except:
+            raise DataError("Opening output file '%s' for writing failed: %s"
+                            % (path, utils.get_error_message()))
+        writer.start('robot', attrs)
+        return writer
+
+    def _close_writer(self, writer):
+        if not writer.closed:
+            writer.end('robot')
+            writer.close()
+
+    def close(self):
+        self.start_errors()
+        for msg in self._errors:
+            self._write_message(msg)
+        self.end_errors()
+        self._close_writer(self._writer)
+
+    def message(self, msg):
+        if self._error_message_is_logged(msg.level):
+            self._errors.append(msg)
+
+    def log_message(self, msg):
+        if self._log_message_is_logged(msg.level):
+            self._write_message(msg)
+
+    def set_log_level(self, level):
+        return self._log_message_is_logged.set_level(level)
+
+    def _write_message(self, msg):
+        attrs = {'timestamp': msg.timestamp, 'level': msg.level}
+        if msg.html:
+            attrs['html'] = 'yes'
+        if msg.linkable:
+            attrs['linkable'] = 'yes'
+        self._writer.element('msg', msg.message, attrs)
+
+    def start_keyword(self, kw):
+        attrs = {'name': kw.name, 'type': kw.type, 'timeout': kw.timeout}
+        self._writer.start('kw', attrs)
+        self._writer.element('doc', kw.doc)
+        self._write_list('arg', [utils.unic(a) for a in kw.args], 'arguments')
+
+    def end_keyword(self, kw):
+        self._write_status(kw)
+        self._writer.end('kw')
+
+    def start_test(self, test):
+        attrs = {'name': test.name, 'timeout': str(test.timeout)}
+        self._writer.start('test', attrs)
+        self._writer.element('doc', test.doc)
+
+    def end_test(self, test):
+        self._write_list('tag', test.tags, 'tags')
+        self._write_status(test, test.message,
+                           extra_attrs={'critical': test.critical})
+        self._writer.end('test')
+
+    def start_suite(self, suite):
+        if not self._writer:
+            self._writer = self._get_writer(*self._writer_args)
+            del self._writer_args
+        self._start_suite(suite)
+
+    def _start_suite(self, suite, extra_attrs=None):
+        attrs = extra_attrs is not None and extra_attrs or {}
+        attrs['name'] = suite.name
+        if suite.source:
+            attrs['source'] = suite.source
+        self._writer.start('suite', attrs)
+        self._writer.element('doc', suite.doc)
+        self._writer.start('metadata')
+        for name, value in suite.get_metadata():
+            self._writer.element('item', value, {'name': name})
+        self._writer.end('metadata')
+
+    def end_suite(self, suite):
+        self._write_status(suite, suite.message)
+        self._writer.end('suite')
+
+    def start_statistics(self, stats):
+        self._writer.start('statistics')
+
+    def end_statistics(self, stats):
+        self._writer.end('statistics')
+
+    def start_total_stats(self, total_stats):
+        self._writer.start('total')
+
+    def end_total_stats(self, total_stats):
+        self._writer.end('total')
+
+    def start_tag_stats(self, tag_stats):
+        self._writer.start('tag')
+
+    def end_tag_stats(self, tag_stats):
+        self._writer.end('tag')
+
+    def start_suite_stats(self, tag_stats):
+        self._writer.start('suite')
+
+    def end_suite_stats(self, tag_stats):
+        self._writer.end('suite')
+
+    def total_stat(self, stat):
+        self._stat(stat)
+
+    def suite_stat(self, stat):
+        self._stat(stat, stat.long_name, attrs={'name': stat.name})
+
+    def tag_stat(self, stat):
+        self._stat(stat, attrs={'info': self._get_tag_stat_info(stat),
+                                'links': self._get_tag_links(stat),
+                                'doc': stat.doc,
+                                'combined': stat.combined})
+
+    def _get_tag_links(self, stat):
+        return ':::'.join(':'.join([title, url]) for url, title in stat.links)
+
+    def _stat(self, stat, name=None, attrs=None):
+        name = name or stat.name
+        attrs = attrs or {}
+        attrs['pass'] = str(stat.passed)
+        attrs['fail'] = str(stat.failed)
+        self._writer.element('stat', name, attrs)
+
+    def _get_tag_stat_info(self, stat):
+        if stat.critical:
+            return 'critical'
+        if stat.non_critical:
+            return 'non-critical'
+        if stat.combined:
+            return 'combined'
+        return ''
+
+    def start_errors(self):
+        self._writer.start('errors')
+
+    def end_errors(self):
+        self._writer.end('errors')
+
+    def _write_list(self, tag, items, container=None):
+        if container:
+            self._writer.start(container)
+        for item in items:
+            self._writer.element(tag, item)
+        if container:
+            self._writer.end(container)
+
+    def _write_status(self, item, message=None, extra_attrs=None):
+        attrs = {'status': item.status, 'starttime': item.starttime,
+                 'endtime': item.endtime}
+        if extra_attrs:
+            attrs.update(extra_attrs)
+        self._writer.element('status', message, attrs)
---
+++ utest/output/test_logger.py
@@ -0,0 +1,241 @@
+import unittest
+
+from robot.utils.asserts import assert_equals, assert_true, assert_false
+
+from robot.output.logger import Logger, CommandLineMonitor
+
+
+class MessageMock:
+
+    def __init__(self, timestamp, level, message):
+        self.timestamp = timestamp
+        self.level = level
+        self.message = message
+
+class LoggerMock:
+
+    def __init__(self, *expected):
+        self.expected = list(expected)
+
+    def message(self, msg):
+        exp_msg, exp_level = self.expected.pop(0)
+        assert_equals(msg.level, exp_level)
+        assert_equals(msg.message, exp_msg)
+        self.msg = msg
+
+    def copy(self):
+        return LoggerMock(*self.expected)
+
+class LoggerMock2(LoggerMock):
+
+    def output_file(self, name, path):
+        self.output_file = (name, path)
+
+    def close(self):
+        self.closed = True
+
+
+class TestLogger(unittest.TestCase):
+
+    def setUp(self):
+        self.logger = Logger()
+        self.logger.disable_automatic_console_logger()
+
+    def test_write_to_one_logger(self):
+        logger = LoggerMock(('Hello, world!', 'INFO'))
+        self.logger.register_logger(logger)
+        self.logger.write('Hello, world!', 'INFO')
+        assert_true(logger.msg.timestamp.startswith('20'))
+
+    def test_write_to_one_logger_with_trace_level(self):
+        logger = LoggerMock(('expected message', 'TRACE'))
+        self.logger.register_logger(logger)
+        self.logger.write('expected message', 'TRACE')
+        assert_true(hasattr(logger, 'msg'))
+
+    def test_write_to_multiple_loggers(self):
+        logger = LoggerMock(('Hello, world!', 'INFO'))
+        logger2 = logger.copy()
+        logger3 = logger.copy()
+        self.logger.register_logger(logger, logger2, logger3)
+        self.logger.message(MessageMock('', 'INFO', 'Hello, world!'))
+        assert_true(logger.msg is logger2.msg)
+        assert_true(logger.msg is logger.msg)
+
+    def test_write_multiple_messages(self):
+        msgs = [('0', 'ERROR'), ('1', 'WARN'), ('2', 'INFO'), ('3', 'DEBUG'), ('4', 'TRACE')]
+        logger = LoggerMock(*msgs)
+        self.logger.register_logger(logger)
+        for msg, level in msgs:
+            self.logger.write(msg, level)
+            assert_equals(logger.msg.message, msg)
+            assert_equals(logger.msg.level, level)
+
+    def test_all_methods(self):
+        logger = LoggerMock2(('Hello, world!', 'INFO'))
+        self.logger.register_logger(logger)
+        self.logger.output_file('name', 'path')
+        self.logger.close()
+        assert_equals(logger.output_file, ('name', 'path'))
+        assert_true(logger.closed)
+
+    def test_registered_logger_does_not_need_all_methods(self):
+        logger = LoggerMock(('Hello, world!', 'INFO'))
+        self.logger.register_logger(logger)
+        self.logger.output_file('name', 'path')
+        self.logger.close()
+
+    def test_close_removes_registered_loggers(self):
+        logger = LoggerMock(('Hello, world!', 'INFO'))
+        logger2 = LoggerMock2(('Hello, world!', 'INFO'))
+        self.logger.register_logger(logger, logger2)
+        self.logger.close()
+        assert_equals(self.logger._loggers.all_loggers(), [])
+
+    def test_registering_file_logger_with_none_path_does_nothing(self):
+        self.logger.register_file_logger('None')
+        assert_equals(self.logger._loggers.all_loggers(), [])
+
+    def test_cached_messages_are_given_to_registered_writers(self):
+        self.logger.write('This is a cached message', 'INFO')
+        self.logger.write('Another cached message', 'TRACE')
+        logger = LoggerMock(('This is a cached message', 'INFO'),
+                            ('Another cached message', 'TRACE'))
+        self.logger.register_logger(logger)
+        assert_equals(logger.msg.message, 'Another cached message')
+
+    def test_message_cache_can_be_turned_off(self):
+        self.logger.disable_message_cache()
+        self.logger.write('This message is not cached', 'INFO')
+        logger = LoggerMock(('', ''))
+        self.logger.register_logger(logger)
+        assert_false(hasattr(logger, 'msg'))
+
+    def test_start_and_end_suite_test_and_keyword(self):
+        class MyLogger:
+            def start_suite(self, suite): self.started_suite = suite
+            def end_suite(self, suite): self.ended_suite = suite
+            def start_test(self, test): self.started_test = test
+            def end_test(self, test): self.ended_test = test
+            def start_keyword(self, keyword): self.started_keyword = keyword
+            def end_keyword(self, keyword): self.ended_keyword = keyword
+        logger = MyLogger()
+        self.logger.register_logger(logger)
+        for name in 'suite', 'test', 'keyword':
+            for stend in 'start', 'end':
+                getattr(self.logger, stend + '_' + name)(name)
+                assert_equals(getattr(logger, stend + 'ed_' + name), name)
+
+    def test_console_logger_is_automatically_registered(self):
+        logger = Logger()
+        assert_true(logger._loggers.all_loggers()[0].start_suite.im_class is CommandLineMonitor)
+
+    def test_loggercollection_is_iterable(self):
+        logger = Logger()
+        for log in logger._loggers:
+            assert_true(log)
+
+    def test_logger_is_iterable(self):
+        logger = Logger()
+        for log in logger:
+            assert_true(log)
+        assert_equals(list(logger), list(logger._loggers))
+
+    def test_automatic_console_logger_can_be_disabled(self):
+        logger = Logger()
+        logger.disable_automatic_console_logger()
+        assert_equals(logger._loggers.all_loggers(), [])
+
+    def test_automatic_console_logger_can_be_disabled_after_registering_logger(self):
+        logger = Logger()
+        mock = LoggerMock()
+        logger.register_logger(mock)
+        logger.disable_automatic_console_logger()
+        self._number_of_registered_loggers_should_be(1, logger)
+        assert_true(logger._loggers.all_loggers()[0].message.im_class is LoggerMock)
+
+    def test_disabling_automatic_logger_multiple_times_has_no_effect(self):
+        logger = Logger()
+        logger.disable_automatic_console_logger()
+        self._number_of_registered_loggers_should_be(0, logger)
+        logger.disable_automatic_console_logger()
+        logger.disable_automatic_console_logger()
+        self._number_of_registered_loggers_should_be(0, logger)
+        logger.register_logger(LoggerMock())
+        logger.disable_automatic_console_logger()
+        self._number_of_registered_loggers_should_be(1, logger)
+
+    def test_registering_console_logger_disables_automatic_console_logger(self):
+        logger = Logger()
+        logger.register_console_logger(width=42)
+        self._number_of_registered_loggers_should_be(1, logger)
+        assert_equals(logger._loggers.all_loggers()[0].start_suite.im_self._width, 42)
+
+    def test_unregister_logger(self):
+        logger1, logger2, logger3 = LoggerMock(), LoggerMock(), LoggerMock()
+        self.logger.register_logger(logger1, logger2, logger3)
+        self.logger.unregister_logger(logger2)
+        self._number_of_registered_loggers_should_be(2)
+        self.logger.unregister_logger(logger3, logger1)
+        self._number_of_registered_loggers_should_be(0)
+
+    def test_unregistering_non_registered_logger_is_ok(self):
+        logger1, logger2 = LoggerMock(), LoggerMock()
+        self.logger.register_logger(logger1)
+        self.logger.unregister_logger(logger2)
+        self.logger.unregister_logger(None)
+
+    def test_registering_context_changing_logger(self):
+        self.logger.register_context_changing_logger(LoggerMock())
+        self._number_of_registered_loggers_should_be(1)
+
+    def test_messages_to_context_chagning_loggers(self):
+        log = LoggerMock(('msg', 'INFO'))
+        self.logger.register_context_changing_logger(log)
+        self.logger.write('msg', 'INFO')
+        assert_true(log.msg is not None)
+
+    def test_start_methods_are_called_first_for_context_changing_loggers(self):
+        class FirstLogger:
+            def start_suite(self, suite): self.suite = suite
+            def start_test(self, test): self.test = test
+            def start_keyword(self, kw): self.kw = kw
+        class SecondLogger:
+            def __init__(self, logger): self._reference = logger
+            def start_suite(self, suite): assert_equals(suite, self._reference.suite)
+            def start_test(self, test): assert_equals(test, self._reference.test)
+            def start_keyword(self, kw): assert_equals(kw, self._reference.kw)
+        log1 = FirstLogger()
+        log2 = SecondLogger(log1)
+        self.logger.register_logger(log2)
+        self.logger.register_context_changing_logger(log1)
+        self.logger.start_suite('Suite')
+        self.logger.start_test('Test')
+        self.logger.start_keyword('Keyword')
+
+    def test_end_methods_are_called_last_for_context_changing_loggers(self):
+        class FirstLogger:
+            def end_suite(self, suite): self.suite = suite
+            def end_test(self, test): self.test = test
+            def end_keyword(self, kw): self.kw = kw
+        class SecondLogger:
+            def __init__(self, logger): self._reference = logger
+            def end_suite(self, suite): self.suite = suite; assert_equals(suite, self._reference.suite)
+            def end_test(self, test): assert_equals(test, self._reference.test)
+            def end_keyword(self, kw): assert_equals(kw, self._reference.kw)
+        log1 = FirstLogger()
+        log2 = SecondLogger(log1)
+        self.logger.register_logger(log1)
+        self.logger.register_context_changing_logger(log2)
+        self.logger.end_suite('Suite')
+        self.logger.end_test('Test')
+        self.logger.end_keyword('Keyword')
+        assert_true(log2.suite is not None)
+
+    def _number_of_registered_loggers_should_be(self, number, logger=None):
+        logger = logger or self.logger
+        assert_equals(len(logger._loggers.all_loggers()), number)
+
+
+if __name__ == "__main__":
+    unittest.main()
