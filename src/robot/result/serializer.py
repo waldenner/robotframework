ac99454c40ea246b335337ad5b030bcc67d0fb21
Mikko Korpela <mikko.korpela@gmail.com>
2011-11-03 15:34:47 +0200

    keywordremover: do not remove setup/teardown that has warnings

---
+++ src/robot/result/serializer.py
@@ -0,0 +1,37 @@
+#  Copyright 2008-2011 Nokia Siemens Networks Oyj
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from robot.output.xmllogger import XmlLogger
+
+from visitor import Visitor
+
+
+class ResultWriter(XmlLogger, Visitor):
+
+    def visit_message(self, msg):
+        self.log_message(msg)
+
+    def close(self):
+        self._writer.end('robot')
+
+
+class ResultSerializer(object):
+
+    def __init__(self, output):
+        self._output = output
+
+    def to_xml(self, suite):
+        logger = ResultWriter(self._output)
+        suite.visit(logger)
+        logger.close()
