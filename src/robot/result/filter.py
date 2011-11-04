5dddd9d8afed41d1cfdfaf7ab493c7b937d7d4c0
Pekka Kl?rck <Pekka Kl?rck@localhost>
2011-11-04 14:14:42 +0200

    free test namespace for tests of the new statistics

---
+++ src/robot/result/filter.py
@@ -0,0 +1,142 @@
+#  Copyright 2008-2011 Nokia Siemens Networks Oyj
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from robot import utils
+from robot.output.loggerhelper import IsLogged
+
+from visitor import Visitor
+from tags import TagPatterns
+
+
+class Filter(Visitor):
+
+    def __init__(self, include_suites=None, include_tests=None,
+                 include_tags=None, exclude_tags=None):
+        self.include_suites = include_suites
+        self.include_tests = include_tests
+        self.include_tags = include_tags
+        self.exclude_tags = exclude_tags
+
+    @utils.setter
+    def include_suites(self, suites):
+        return _SuiteNameFilter(suites) \
+            if not isinstance(suites, _SuiteNameFilter) else suites
+
+    @utils.setter
+    def include_tests(self, tests):
+        return _TestNameFilter(tests) \
+            if not isinstance(tests, _TestNameFilter) else tests
+
+    @utils.setter
+    def include_tags(self, tags):
+        return TagPatterns(tags) if not isinstance(tags, TagPatterns) else tags
+
+    @utils.setter
+    def exclude_tags(self, tags):
+        return TagPatterns(tags) if not isinstance(tags, TagPatterns) else tags
+
+    def start_suite(self, suite):
+        if not self:
+            return False
+        suite.starttime = suite.endtime = 'N/A' # TODO: is this too eager?
+        if self.include_suites:
+            return self._filter_by_suite_name(suite)
+        if self.include_tests:
+            suite.tests = list(self._filter(suite, self._included_by_test_name))
+        if self.include_tags:
+            suite.tests = list(self._filter(suite, self._included_by_tags))
+        if self.exclude_tags:
+            suite.tests = list(self._filter(suite, self._not_excluded_by_tags))
+        return bool(suite.suites)
+
+    def _filter_by_suite_name(self, suite):
+        if self.include_suites.match(suite):
+            suite.visit(Filter(include_suites=[],
+                               include_tests=self.include_tests,
+                               include_tags=self.include_tags,
+                               exclude_tags=self.exclude_tags))
+            return False
+        suite.tests = []
+        return True
+
+    def _filter(self, suite, filter):
+        for test in suite.tests:
+            if filter(test):
+                yield test
+
+    def _included_by_test_name(self, test):
+        return self.include_tests.match(test)
+
+    def _included_by_tags(self, test):
+        return self.include_tags.match(test.tags)
+
+    def _not_excluded_by_tags(self, test):
+        return not self.exclude_tags.match(test.tags)
+
+    def end_suite(self, suite):
+        suite.suites = [s for s in suite.suites if s.test_count]
+
+    def visit_test(self, test):
+        pass
+
+    def visit_keyword(self, keyword):
+        pass
+
+    def __nonzero__(self):
+        return bool(self.include_suites or self.include_tests or
+                    self.include_tags or self.exclude_tags)
+
+
+class _NameFilter(object):
+
+    def __init__(self, patterns):
+        if isinstance(patterns, basestring):
+            patterns = [patterns]
+        self._patterns = patterns
+
+    def match(self, item):
+        return self._match(item.name) or self._match_longname(item.longname)
+
+    def _match(self, name):
+        return utils.matches_any(name, self._patterns, ignore=['_'])
+
+    def __nonzero__(self):
+        return bool(self._patterns)
+
+
+class _SuiteNameFilter(_NameFilter):
+
+    def _match_longname(self, name):
+        while '.' in name:
+            if self._match(name):
+                return True
+            name = name.split('.', 1)[1]
+        return False
+
+
+class _TestNameFilter(_NameFilter):
+
+    def _match_longname(self, name):
+        return self._match(name)
+
+
+class MessageFilter(Visitor):
+
+    def __init__(self, loglevel):
+        loglevel = loglevel or 'trace' #TODO: is this the correct default?
+        self._is_logged = IsLogged(loglevel)
+
+    def start_keyword(self, keyword):
+        keyword.messages = [msg for msg in keyword.messages
+                            if self._is_logged(msg.level)]
---
+++ utest/parsing/test_tsvreader.py
@@ -0,0 +1,79 @@
+import sys
+import unittest
+from StringIO import StringIO
+
+if __name__ == "__main__":
+    sys.path.insert(0, "../../../src")
+
+from robot.parsing.tsvreader import TsvReader
+from robot.parsing.model import TestCaseFile
+from robot.parsing.populators import FromFilePopulator
+from robot.utils.asserts import *
+import robot.parsing.populators
+
+
+class TestTsvParser(unittest.TestCase):
+
+    def setUp(self):
+        self.tcf = TestCaseFile()
+        self._orig_curdir = robot.parsing.populators.PROCESS_CURDIR
+        robot.parsing.populators.PROCESS_CURDIR = False
+
+    def tearDown(self):
+        robot.parsing.populators.PROCESS_CURDIR = self._orig_curdir
+
+    def test_start_table(self):
+        tsv = StringIO('''*SettING*\t*  Value  *\t*V*
+***Variable
+
+*Not*Table*
+
+Keyword*\tNot a table because doesn't start with '*'
+
+*******************T*e*s*t*********C*a*s*e************\t***********\t******\t*
+''')
+        TsvReader().read(tsv, FromFilePopulator(self.tcf))
+        assert_equals(self.tcf.setting_table.name, 'SettING')
+        assert_equals(self.tcf.setting_table.header, ['SettING','Value','V'])
+
+    def test_rows(self):
+        tsv = StringIO('''Ignored text before tables...
+Mote\tignored\text
+*Setting*\t*Value*\t*Value*
+Document\tWhatever\t\t\\\t
+Default Tags\tt1\tt2\tt3\t\t
+
+*Variable*\tWhatever
+\\ \\ 2 escaped spaces before and after \\ \\\t\\ \\ value \\ \\
+    
+''')
+        TsvReader().read(tsv, FromFilePopulator(self.tcf))
+        assert_equal(self.tcf.setting_table.doc.value, 'Whatever  ')
+        assert_equal(self.tcf.setting_table.default_tags.value, ['t1','t2','t3'])
+        assert_equal(self.tcf.variable_table.variables[0].name, '\\ \\ 2 escaped spaces before and after \\ \\')
+        assert_equal(self.tcf.variable_table.variables[0].value, ['\\ \\ value \\ \\'])
+
+    
+    def test_quotes(self):
+        tsv = StringIO('''*Variable*\t*Value*
+${v}\tHello
+${v}\t"Hello"
+${v}\t"""Hello"""
+${v}\t"""""Hello"""""
+${v}\t"Hel""lo"
+${v}\t"""Hel "" """" lo"""""""
+${v}\t"Hello
+${v}\tHello"
+''')
+        TsvReader().read(tsv, FromFilePopulator(self.tcf))
+        actual = [ variable for variable in self.tcf.variable_table.variables ]
+        expected = ['Hello','Hello','"Hello"','""Hello""','Hel"lo',
+                    '"Hel " "" lo"""','"Hello','Hello"']
+        assert_equals(len(actual), len(expected))
+        for act, exp in zip(actual, expected):
+            assert_equals(act.name, '${v}')
+            assert_equals(act.value, [exp])
+
+
+if __name__ == '__main__':
+    unittest.main()
