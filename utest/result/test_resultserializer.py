8e874224f708fa3f029f41c5c19f1ad1d6c761e1
Mikko Korpela <mikko.korpela@gmail.com>
2011-11-09 13:03:39 +0200

    utils.setter with attribute name

---
+++ utest/result/test_resultserializer.py
@@ -0,0 +1,73 @@
+from itertools import chain
+import unittest
+from StringIO import StringIO
+from xml.etree.ElementTree import XML
+from xml.etree.ElementTree import tostring
+from robot.reporting.outputparser import OutputParser
+
+from robot.result.builders import ResultFromXML
+from robot.result.serializer import RebotXMLWriter
+from robot.result.datamodel import DatamodelVisitor
+from robot.utils.asserts import assert_equals
+
+from test_resultbuilder import GOLDEN_XML, GOLDEN_XML_TWICE
+
+
+class TestResultSerializer(unittest.TestCase):
+
+    def test_single_result_serialization(self):
+        output = StringIO()
+        ResultFromXML(StringIO(GOLDEN_XML)).visit(RebotXMLWriter(output))
+        self._assert_xml_content(self._xml_lines(output.getvalue()),
+                                 self._xml_lines(GOLDEN_XML))
+
+    def _xml_lines(self, text):
+        return tostring(XML(text)).splitlines()
+
+    def _assert_xml_content(self, actual, expected):
+        assert_equals(len(actual), len(expected))
+        for index, (act, exp) in enumerate(zip(actual, expected)[2:]):
+            assert_equals(act, exp.strip(), 'Different values on line %d' % index)
+
+    def test_combining_results(self):
+        output = StringIO()
+        result = ResultFromXML(StringIO(GOLDEN_XML), StringIO(GOLDEN_XML))
+        result.visit(RebotXMLWriter(output))
+        self._assert_xml_content(self._xml_lines(output.getvalue()),
+                                 self._xml_lines(GOLDEN_XML_TWICE))
+
+class TestResultJSONSerializer(unittest.TestCase):
+
+    def setUp(self):
+        output_parser = OutputParser()
+        output_parser._parse_fileobj(StringIO(GOLDEN_XML))
+        self._expected = output_parser._get_data_model()._robot_data
+        visitor = DatamodelVisitor(ResultFromXML(StringIO(GOLDEN_XML)))
+        self._datamodel = visitor.datamodel
+
+    def test_datamodel_suite(self):
+        self._equals('suite')
+
+    def test_datamodel_basemillis(self):
+        self._equals('baseMillis')
+
+    def test_datamodel_strings(self):
+        self._equals('strings')
+
+    def test_datamodel_statistics(self):
+        self._equals('stats')
+
+    def test_datamodel_errors(self):
+        self._equals('errors')
+
+    def _equals(self, key):
+        if isinstance(self._expected[key], list):
+            assert_equals(len(self._expected[key]), len(self._datamodel[key]))
+            for exp, act in zip(self._expected[key], self._datamodel[key]):
+                assert_equals(exp, act)
+        else:
+            assert_equals(self._expected[key], self._datamodel[key])
+
+
+if __name__ == '__main__':
+    unittest.main()
