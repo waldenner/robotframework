355d8fa18dd1110aff36675b2aa96095d417a1e3
Pekka Kl?rck <Pekka Kl?rck@localhost>
2011-11-29 11:53:28 +0200

    Removed parsingcontext.Stats because it wasn't needed anymore now that suite teardown failures are handled earlier.

---
+++ utest/result/test_resultserializer.py
@@ -0,0 +1,37 @@
+import unittest
+from StringIO import StringIO
+from xml.etree.ElementTree import XML
+from xml.etree.ElementTree import tostring
+
+from robot.result.builders import ResultFromXML
+from robot.result.serializer import RebotXMLWriter
+from robot.utils.asserts import assert_equals
+
+from test_resultbuilder import GOLDEN_XML, GOLDEN_XML_TWICE
+
+
+class TestResultSerializer(unittest.TestCase):
+
+    def test_single_result_serialization(self):
+        output = StringIO()
+        ResultFromXML(StringIO(GOLDEN_XML)).visit(RebotXMLWriter(output))
+        self._assert_xml_content(self._xml_lines(output.getvalue()),
+                                 self._xml_lines(GOLDEN_XML))
+
+    def _xml_lines(self, text):
+        return tostring(XML(text)).splitlines()
+
+    def _assert_xml_content(self, actual, expected):
+        assert_equals(len(actual), len(expected))
+        for index, (act, exp) in enumerate(zip(actual, expected)[2:]):
+            assert_equals(act, exp.strip(), 'Different values on line %d' % index)
+
+    def test_combining_results(self):
+        output = StringIO()
+        result = ResultFromXML(StringIO(GOLDEN_XML), StringIO(GOLDEN_XML))
+        result.visit(RebotXMLWriter(output))
+        self._assert_xml_content(self._xml_lines(output.getvalue()),
+                                 self._xml_lines(GOLDEN_XML_TWICE))
+
+if __name__ == '__main__':
+    unittest.main()
