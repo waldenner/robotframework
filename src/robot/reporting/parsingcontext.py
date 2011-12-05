515f460d0391c3da930629cf8380f11532c850cb
Pekka Kl?rck <Pekka Kl?rck@localhost>
2011-12-05 14:56:30 +0200

    New JS model: html formatting of docs and suite metadata

---
+++ src/robot/reporting/parsingcontext.py
@@ -0,0 +1,137 @@
+#  Copyright 2008-2011 Nokia Siemens Networks Oyj
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import os.path
+from operator import itemgetter
+
+from robot import utils
+
+
+class Context(object):
+
+    def __init__(self, log_path=None, split_log=False):
+        self._main_text_cache = TextCache()
+        self._current_texts = self._main_text_cache
+        self._split_text_caches = []
+        self.basemillis = 0
+        self._links = {}
+        self._split_log = split_log
+        self.split_results = []
+        self._log_path = log_path
+
+    def get_rel_log_path(self, path):
+        if path and os.path.exists(path) and self._log_path:
+            return utils.get_link_path(path, os.path.dirname(self._log_path))
+        return ''
+
+    def get_id(self, value):
+        if value is None:
+            return None
+        if isinstance(value, basestring):
+            return self._get_text_id(value)
+        if isinstance(value, (int, long)):
+            return value
+        raise TypeError('Unsupported type %s' % type(value))
+
+    def _get_text_id(self, text):
+        return self._current_texts.add(text)
+
+    def dump_texts(self):
+        return self._current_texts.dump()
+
+    def timestamp(self, time):
+        if time == 'N/A':
+            return None
+        # Must use `long` and not `int` below due to this IronPython bug:
+        # http://ironpython.codeplex.com/workitem/31549
+        millis = long(round(utils.timestamp_to_secs(time) * 1000))
+        if not self.basemillis:
+            self.basemillis = millis
+        return millis - self.basemillis
+
+    def start_test(self):
+        if self._split_log:
+            self._split_text_caches.append(TextCache())
+
+    def end_test(self, kw_data=None):
+        if self._split_log and kw_data:
+            self.split_results.append((kw_data, self._split_text_caches[-1].dump()))
+            return len(self.split_results)
+        return kw_data
+
+    def start_keyword(self):
+        if self._split_log:
+            self._current_texts = self._split_text_caches[-1]
+
+    def end_keyword(self, is_top_level_keyword=False):
+        if self._split_log and is_top_level_keyword:
+            self._current_texts = self._main_text_cache
+
+    def start_suite_setup_or_teardown(self):
+        return self.start_test()
+
+    def end_suite_setup_or_teardown(self, kw_data=None):
+        return self.end_test(kw_data)
+
+    def create_link_to_current_location(self, msg):
+        self._links[self._link_key(msg)] = msg.parent.id
+
+    def link_to(self, msg):
+        return self._links[self._link_key(msg)]
+
+    def _link_key(self, msg):
+        return (msg.message, msg.level, msg.timestamp)
+
+
+class TextIndex(long):
+
+    def __str__(self):
+        return long.__str__(self).rstrip('L')  # Jython adds L at the end
+
+
+class TextCache(object):
+    _compress_threshold = 80
+    _use_compressed_threshold = 1.1
+
+    def __init__(self):
+        self.texts = {'*': TextIndex(0)}
+        self.index = 1
+
+    def add(self, text):
+        if not text:
+            return TextIndex(0)
+        text = self._encode(text)
+        if text not in self.texts:
+            self.texts[text] = TextIndex(self.index)
+            self.index += 1
+        return self.texts[text]
+
+    def _encode(self, text):
+        raw = self._raw(text)
+        if raw in self.texts or len(raw) < self._compress_threshold:
+            return raw
+        compressed = self._compress(text)
+        if len(compressed) * self._use_compressed_threshold < len(raw):
+            return compressed
+        return raw
+
+    def _compress(self, text):
+        return utils.compress_text(text)
+
+    def _raw(self, text):
+        return '*'+text
+
+    def dump(self):
+        return [item[0] for item in sorted(self.texts.iteritems(),
+                                           key=itemgetter(1))]
---
+++ utest/reporting/test_reporting.py
@@ -0,0 +1,163 @@
+from StringIO import StringIO
+import os
+import unittest
+from robot.reporting.resultwriter import ResultWriter, Result
+from robot.output import LOGGER
+
+from robot.result.executionresult import ExecutionResult
+from robot.result.testsuite import TestSuite
+from robot.utils.asserts import assert_true, assert_equals
+
+
+LOGGER.disable_automatic_console_logger()
+
+
+class TestReporting(unittest.TestCase, ResultWriter):
+
+    EXPECTED_SUITE_NAME = 'My Suite Name'
+    EXPECTED_TEST_NAME  = 'My Test Name'
+    EXPECTED_KEYWORD_NAME = 'My Keyword Name'
+    EXPECTED_FAILING_TEST = 'My Failing Test'
+    EXPECTED_DEBUG_MESSAGE = '1111DEBUG777'
+
+    def setUp(self):
+        self._settings = lambda:0
+        self._settings.log = None
+        self._settings.log_config = None
+        self._settings.split_log = False
+        self._settings.report = None
+        self._settings.report_config = None
+        self._settings.output = None
+        self._settings.xunit = None
+        self._settings.status_rc = True
+        self._settings.suite_config = {}
+        self._settings.statistics_config = {}
+        self._create_default_suite()
+
+    def _create_default_suite(self):
+        root_suite = TestSuite(name=self.EXPECTED_SUITE_NAME)
+        root_suite.tests.create(name=self.EXPECTED_TEST_NAME).\
+                    keywords.create(name=self.EXPECTED_KEYWORD_NAME,
+                                    status='PASS')
+        root_suite.tests.create(name=self.EXPECTED_FAILING_TEST).\
+                    keywords.create(name=self.EXPECTED_KEYWORD_NAME).\
+                        messages.create(message=self.EXPECTED_DEBUG_MESSAGE,
+                                        level='DEBUG',
+                                        timestamp='20201212 12:12:12.000')
+        self._result = Result(self._settings, None)
+        self._result._model = ExecutionResult(root_suite)
+
+    def test_generate_report_and_log(self):
+        self._settings.log = ClosingOutput()
+        self._settings.report = ClosingOutput()
+        self.write_results()
+        self._verify_log()
+        self._verify_report()
+
+    def _verify_log(self):
+        log = self._settings.log.getvalue()
+        assert_true(self.EXPECTED_KEYWORD_NAME in log)
+        assert_true(self.EXPECTED_SUITE_NAME in log)
+        assert_true(self.EXPECTED_TEST_NAME in log)
+        assert_true(self.EXPECTED_FAILING_TEST in log)
+
+    def _verify_report(self):
+        report = self._settings.report.getvalue()
+        assert_true(self.EXPECTED_KEYWORD_NAME not in report)
+        assert_true(self.EXPECTED_SUITE_NAME in report)
+        assert_true(self.EXPECTED_TEST_NAME in report)
+        assert_true(self.EXPECTED_FAILING_TEST in report)
+
+    def test_no_generation(self):
+        self._result._model = None
+        self.write_results()
+        assert_equals(self._result._model, None)
+
+    def test_only_log(self):
+        self._settings.log = ClosingOutput()
+        self.write_results()
+        self._verify_log()
+
+    def test_only_report(self):
+        self._settings.report = ClosingOutput()
+        self.write_results()
+        self._verify_report()
+
+    def test_only_xunit(self):
+        self._settings.xunit = ClosingOutput()
+        self.write_results()
+        self._verify_xunit()
+
+    def test_only_output_generation(self):
+        self._settings.output = ClosingOutput()
+        self.write_results()
+        self._verify_output()
+
+    def test_generate_all(self):
+        self._settings.log = ClosingOutput()
+        self._settings.report = ClosingOutput()
+        self._settings.xunit = ClosingOutput()
+        self._settings.output = ClosingOutput()
+        self.write_results()
+        self._verify_log()
+        self._verify_report()
+        self._verify_xunit()
+        self._verify_output()
+
+    def _verify_xunit(self):
+        xunit = self._settings.xunit.getvalue()
+        assert_true(self.EXPECTED_DEBUG_MESSAGE in xunit)
+
+    def _verify_output(self):
+        assert_true(self._settings.output.getvalue())
+
+    #TODO: Find a way to test split_log
+    def _test_split_tests(self):
+        self._settings.split_log = True
+        self._settings.log = ClosingOutput()
+        self.write_results()
+        self._verify_log()
+
+if os.name == 'java':
+    import java.io.OutputStream
+    import java.lang.String
+
+    class ClosingOutput(java.io.OutputStream):
+        def __init__(self):
+            self._output = StringIO()
+
+        __enter__ = lambda *args: 0
+        __exit__ = lambda self, *args: self.close()
+
+        def write(self, *args):
+            self._output.write(java.lang.String(args[0]))
+
+        def close(self):
+            self.value = self._output.getvalue()
+            self._output.close()
+
+        def getvalue(self):
+            return self.value
+else:
+
+    class ClosingOutput(object):
+
+        def __init__(self):
+            self._output = StringIO()
+
+        __enter__= lambda *args: 0
+        __exit__ = lambda self, *args: self.close()
+
+        def write(self, data):
+            self._output.write(data)
+
+        def close(self):
+            self.value = self._output.getvalue()
+            self._output.close()
+
+        def getvalue(self):
+            return self.value
+
+
+if __name__ == '__main__':
+    unittest.main()
