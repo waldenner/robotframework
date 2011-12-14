63b1bf3e0f7486f7ea68f9e08d0e386a11dcd9dc
Pekka Kl?rck <Pekka Kl?rck@localhost>
2011-12-14 10:37:51 +0200

    RebotXMLWriter -> OutputWriter

---
+++ src/robot/reporting/builders.py
@@ -0,0 +1,107 @@
+#  Copyright 2008-2011 Nokia Siemens Networks Oyj
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from __future__ import with_statement
+import os
+import os.path
+import codecs
+
+from robot.errors import DataError
+from robot.output import LOGGER
+from robot.result.outputwriter import OutputWriter
+
+from .jswriter import SplitLogWriter
+from .xunitwriter import XUnitWriter
+from .htmlfilewriter import HtmlFileWriter
+
+
+class OutputBuilder(object):
+
+    def __init__(self, model):
+        self._model = model
+
+    def build(self, path):
+        try:
+            writer = OutputWriter(path)
+        except DataError, err:
+            LOGGER.error(unicode(err))
+        else:
+            self._model.visit(writer)
+            LOGGER.output_file('Output', path)
+
+
+class XUnitBuilder(object):
+
+    def __init__(self, model):
+        self._model = model
+
+    def build(self, path):
+        try:
+            writer = XUnitWriter(path)
+        except EnvironmentError, err:
+            LOGGER.error("Opening XUnit result file '%s' failed: %s"
+                         % (path, err.strerror))
+        else:
+            self._model.visit(writer)
+            writer.close()
+            LOGGER.output_file('XUnit', path)
+
+
+class _HTMLFileBuilder(object):
+
+    def __init__(self, js_model):
+        self._js_model = js_model
+
+    def _write_file(self, output, config, template):
+        outfile = codecs.open(output, 'wb', encoding='UTF-8') \
+            if isinstance(output, basestring) else output  # isinstance is unit test hook
+        with outfile:
+            writer = HtmlFileWriter(outfile, self._js_model, config)
+            writer.write(template)
+
+
+class LogBuilder(_HTMLFileBuilder):
+
+    def build(self, output, config):
+        try:
+            self._write_file(output, config, 'log.html')
+            self._write_split_logs_if_needed(output)
+        except EnvironmentError, err:
+            # Cannot use err.filename due to http://bugs.jython.org/issue1825
+            # and thus error has wrong file name if writing split log fails.
+            LOGGER.error("Writing log file '%s' failed: %s" % (output, err.strerror))
+        else:
+            LOGGER.output_file('Log', output)
+
+    def _write_split_logs_if_needed(self, output):
+        base = os.path.splitext(output)[0] if isinstance(output, basestring) else ''
+        for index, (keywords, strings) in enumerate(self._js_model.split_results):
+            index += 1  # enumerate accepts start index only in Py 2.6+
+            self._write_split_log(index, keywords, strings, '%s-%d.js' % (base, index))
+
+    def _write_split_log(self, index, keywords, strings, path):
+        with codecs.open(path, 'wb', encoding='UTF-8') as outfile:
+            writer = SplitLogWriter(outfile)
+            writer.write(keywords, strings, index, os.path.basename(path))
+
+
+class ReportBuilder(_HTMLFileBuilder):
+
+    def build(self, path, config):
+        try:
+            self._write_file(path, config, 'report.html')
+        except EnvironmentError, err:
+            LOGGER.error("Writing report file '%s' failed: %s" % (path, err.strerror))
+        else:
+            LOGGER.output_file('Report', path)
---
+++ utest/result/test_resultserializer.py
@@ -0,0 +1,96 @@
+import unittest
+import os
+from StringIO import StringIO
+from xml.etree.ElementTree import XML
+from xml.etree.ElementTree import tostring
+
+from robot.result import ResultFromXml
+from robot.result.outputwriter import OutputWriter
+from robot.utils.pyxmlwriter import XmlWriter
+from robot.utils.asserts import assert_equals
+
+from test_resultbuilder import GOLDEN_XML, GOLDEN_XML_TWICE
+
+
+class StreamXmlWriter(XmlWriter):
+
+    def _create_output(self, output):
+        return output
+
+    def close(self):
+        pass
+
+
+if os.name == 'java':
+    from java.io import Writer
+    from array import array
+    from robot.utils.jyxmlwriter import XmlWriter
+
+    class StreamXmlWriter(XmlWriter):
+
+        def _create_output(self, output):
+            return StreamOutputWriter(output)
+
+        def close(self):
+            pass
+
+    class StreamOutputWriter(Writer):
+
+        def __init__(self, output):
+            self._output = output
+
+        def close(self):
+            pass
+
+        def flush(self):
+            pass
+
+        def write(self, value, offset=None, length=None):
+            # There are 5 overloaded version of #write() in java.io.Writer,
+            # the three that TransformHandler uses are handled below.
+            self._output.write(self._content(value, offset, length))
+
+        def _content(self, value, offset, length):
+            if isinstance(value, array):
+                return value[offset:offset+length].tostring()
+            if isinstance(value, int):
+                return unichr(value)
+            return value
+
+
+class TestableOutputWriter(OutputWriter):
+
+    def _get_writer(self, output, generator):
+        writer = StreamXmlWriter(output)
+        writer.start('robot')
+        return writer
+
+
+class TestResultSerializer(unittest.TestCase):
+
+    def _create_writer(self, output):
+        return TestableOutputWriter(output)
+
+    def test_single_result_serialization(self):
+        output = StringIO()
+        ResultFromXml(StringIO(GOLDEN_XML)).visit(self._create_writer(output))
+        self._assert_xml_content(self._xml_lines(output.getvalue()),
+                                 self._xml_lines(GOLDEN_XML))
+
+    def _xml_lines(self, text):
+        return tostring(XML(text)).splitlines()
+
+    def _assert_xml_content(self, actual, expected):
+        assert_equals(len(actual), len(expected))
+        for index, (act, exp) in enumerate(zip(actual, expected)[2:]):
+            assert_equals(act, exp.strip(), 'Different values on line %d' % index)
+
+    def test_combining_results(self):
+        output = StringIO()
+        result = ResultFromXml(StringIO(GOLDEN_XML), StringIO(GOLDEN_XML))
+        result.visit(self._create_writer(output))
+        self._assert_xml_content(self._xml_lines(output.getvalue()),
+                                 self._xml_lines(GOLDEN_XML_TWICE))
+
+if __name__ == '__main__':
+    unittest.main()
