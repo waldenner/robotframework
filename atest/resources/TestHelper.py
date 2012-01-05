e17cfcf92563d35670560f1ab0b6848e8bce2f63
Pekka Kl?rck <Pekka Kl?rck@localhost>
2012-01-05 15:45:01 +0200

    test data format from html to txt

---
+++ atest/resources/TestHelper.py
@@ -0,0 +1,43 @@
+import os
+import sys
+from stat import S_IREAD, S_IWRITE
+
+
+class TestHelper:
+    
+    def set_read_only(self, path):
+        os.chmod(path, S_IREAD)
+
+    def set_read_write(self, path):
+        os.chmod(path, S_IREAD | S_IWRITE)
+
+    def get_output_name(self, *datasources):
+        if not datasources:
+            raise RuntimeError('One or more data sources must be given!')
+        if len(datasources) == 1:
+            return self._get_name(datasources[0])
+        return '_'.join(self._get_name(source) for source in datasources)
+        
+    def _get_name(self, path):
+        return os.path.splitext(os.path.basename(path))[0]
+    
+    def should_contain_item_x_times(self, string, item, count):
+        if string.count(item) != int(count):
+            raise AssertionError("'%s' does not contain '%s' '%s' "
+                                 "times!" % (string, item, count))
+
+    def get_splitted_full_name(self, full_name, splitlevel):
+        splitlevel = int(splitlevel)
+        parts = full_name.split('.')
+        if splitlevel > 0 and splitlevel <= len(parts):
+            parts = parts[splitlevel:]
+        return '.'.join(parts)
+
+    def running_on_jython(self, interpreter):
+        return 'jython' in interpreter
+
+    def running_on_python(self, interpreter):
+        return not self.running_on_jython(interpreter)
+
+    def running_on_linux(self):
+        return 'linux' in sys.platform
