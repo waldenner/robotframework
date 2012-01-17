1c5366be22a7b7b666f3929b9c2a651ceadd8994
Janne H?rk?nen <jth@reaktor.fi>
2012-01-17 15:07:13 +0200

    writer: encode all output with UTF-8

---
+++ atest/robot/tidy/TidyLib.py
@@ -0,0 +1,56 @@
+from __future__ import with_statement
+
+import os
+from os.path import abspath, dirname, join
+from subprocess import call, STDOUT
+import tempfile
+
+from robot import DataError
+from robot.utils.asserts import assert_equals
+from robot.tidy import TidyCommandLine
+import robot.tidy
+
+ROBOT_SRC = join(dirname(abspath(__file__)), '..', '..', '..', 'src')
+
+
+class TidyLib(object):
+
+    def __init__(self, interpreter):
+        self._cmd = [interpreter, '-m', 'robot.tidy']
+        path_var = 'PYTHONPATH' if 'python' in interpreter else 'JYTHONPATH'
+        self._env = os.environ
+        self._env.update({path_var: ROBOT_SRC})
+
+    def run_tidy_and_return_output(self, options, input):
+        """Runs tidy in the operating system and returns output."""
+        options = options.split(' ') if options else []
+        with tempfile.TemporaryFile() as output:
+            rc = call(self._cmd + options + [self._path(input)],
+                      stdout=output, stderr=STDOUT,
+                      env=self._env)
+            output.seek(0)
+            content = output.read()
+            if rc:
+                raise RuntimeError(content)
+            return content
+
+    def run_tidy_and_check_result(self, options, input, expected):
+        """Runs tidy and checks that output matches content of file `expected`."""
+        result = self.run_tidy_and_return_output(options, input)
+        self._assert_result(result, open(self._path(expected)).read())
+
+    def _path(self, path):
+        return path.replace('/', os.sep)
+
+    def _assert_result(self, result, expected):
+        for line1, line2 in zip(result.split(), expected.split()):
+            msg = "\n%s\n!=\n%s\n" % (result, expected)
+            assert_equals(repr(unicode(line1)), repr(unicode(line2)), msg)
+
+    def run_tidy(self, argument_string):
+        """Runs tidy programmatically. Fails if there are any expections."""
+        runner = TidyCommandLine(robot.tidy.__doc__)
+        try:
+            runner.run([str(a) for a in argument_string.split()])
+        except DataError, err:
+            raise RuntimeError(unicode(err))
---
+++ utest/writer/test_formatters.py
@@ -0,0 +1,163 @@
+import unittest
+from robot.parsing.model import TestCaseTable, TestCaseFileSettingTable
+
+from robot.writer.formatters import TxtFormatter, HtmlFormatter, TsvFormatter, PipeFormatter
+from robot.writer.tableformatters import RowSplitter, HtmlCell
+from robot.utils.asserts import assert_equals, assert_true
+
+
+class TestRowSplitter(unittest.TestCase):
+
+    def test_escaping_empty_cells_at_eol(self):
+        formatter = RowSplitter(cols=3)
+        assert_equals(formatter.split(['Some', 'text', '', 'with empty'], 0),
+                                       [['Some', 'text', '${EMPTY}'],
+                                        ['...', 'with empty']])
+
+    def test_splitting_inside_comment(self):
+        formatter = RowSplitter(cols=3)
+        assert_equals(formatter.split(['Kw', 'Arg', '#Comment in', 'many cells'], 0),
+                      [['Kw', 'Arg', '#Comment in'], ['...', '#many cells']])
+
+
+class TestTxtFormatter(unittest.TestCase):
+
+    def test_escaping_whitespace(self):
+        assert_equals(TxtFormatter()._escape(['so  me']), ['so \ me'])
+
+    def test_replacing_newlines(self):
+        assert_equals(TxtFormatter()._escape(['so\nme']), ['so me'])
+
+
+class TestPipeFormatter(unittest.TestCase):
+
+    def test_escaping_pipes(self):
+        assert_equals(PipeFormatter()._escape(['so | me']), ['so \\| me'])
+        assert_equals(PipeFormatter()._escape(['|so|me|']), ['|so|me|'])
+        assert_equals(PipeFormatter()._escape(['so |']), ['so \\|'])
+        assert_equals(PipeFormatter()._escape(['| so']), ['\\| so'])
+
+    def test_empty_cell(self):
+        settings = TestCaseFileSettingTable(None)
+        settings.force_tags.value = ['f1', '', 'f3']
+        assert_equals(list(PipeFormatter().setting_rows(settings))[0],
+                      ['Force Tags    ', 'f1', '  ', 'f3'])
+
+
+class TestTsvFormatter(unittest.TestCase):
+
+    def test_replacing_newlines(self):
+        assert_equals(TsvFormatter()._format_row(['so\nme'])[0], 'so me')
+
+
+class TestHtmlFormatter(unittest.TestCase):
+
+    def setUp(self):
+        self._formatter = HtmlFormatter()
+
+    def test_setting_table_doc(self):
+        table = TestCaseFileSettingTable(None)
+        table.set_header('Settings')
+        table.doc.value = 'Some documentation'
+        formatted = list(self._formatter.setting_rows(table))
+        assert_equals(self._rows_to_text(formatted),
+                      [['Documentation', 'Some documentation']])
+        assert_equals(formatted[0][1].attributes,
+                      {'colspan': '4', 'class': 'colspan4'})
+
+    def test_test_name_row_formatting(self):
+        table = self._create_test_table()
+        test = table.add('A Test')
+        test.tags.value = ['t1', 't2', 't3', 't4']
+        formatted = self._rows(table)
+        assert_equals(len(formatted), 2)
+        assert_equals(formatted[0], ['<a name="test_A Test">A Test</a>', '[Tags]', 't1', 't2', 't3'])
+        assert_equals(formatted[1], ['', '...', 't4', '', ''])
+
+    def test_test_documentation_colspan(self):
+        table = self._create_test_table()
+        test = table.add('Test')
+        test.doc.value = 'Some doc'
+        assert_equals(self._rows(table)[0],
+            ['<a name="test_Test">Test</a>', '[Documentation]', 'Some doc'])
+        assert_equals(list(self._formatter.test_rows(table))[0][2].attributes,
+                      {'colspan': '3', 'class': 'colspan3'})
+
+    def test_test_documentation_with_comment(self):
+        table = self._create_test_table()
+        test = table.add('Test')
+        test.doc.value = 'Some doc'
+        test.doc._set_comment('a comment')
+        assert_equals(self._rows(table)[0],
+            ['<a name="test_Test">Test</a>', '[Documentation]', 'Some doc', '# a comment', ''])
+        assert_equals(list(self._formatter.test_rows(table))[0][2].attributes, {})
+
+    def test_testcase_table_custom_headers(self):
+        self._check_header_length([], 1)
+        self._check_header_length(['a', 'b', 'ceee dee'], 4)
+        self._check_header_length(['akjsakjskjd kjsda kdjs'], 2)
+        self._check_header_length([str(i) for i in range(1000)], 1001)
+
+    def test_header_width_matches_widest_row(self):
+        table = self._create_test_table(['h', 'e'])
+        test = table.add('Some test')
+        test.add_step(['kw', 'arg1', 'arg2', 'arg3'])
+        assert_equals(len(self._formatter.header_row(table)), 5)
+
+    def _check_header_length(self, headers, expected_length):
+        table = self._create_test_table(headers)
+        assert_equals(len(self._formatter.header_row(table)), expected_length)
+
+    def test_testcase_table_header_colspan(self):
+        self._assert_header_colspan([], 5)
+        self._assert_header_colspan(['a', 'b'], 1)
+
+    def _assert_header_colspan(self, header, expected_colspan):
+        table = self._create_test_table(header)
+        assert_equals(self._formatter.header_row(table)[0].attributes['colspan'],
+                      str(expected_colspan))
+
+    def test_number_of_columns_is_max_of_header_and_row_widths(self):
+        table = self._create_test_table(['a', 'b'])
+        test = table.add('Test')
+        test.add_step(['Log Many', 'kukka', 'nen'])
+        self._check_row_lengths(table, 4)
+        table = self._create_test_table(['a', 'b', 'c'])
+        test = table.add('Test')
+        test.add_step(['No Operation'])
+        self._check_row_lengths(table, 4)
+
+    def _check_row_lengths(self, table, expected_length):
+        rows = list(self._formatter.test_rows(table))
+        assert_true(len(rows) > 0)
+        for row in rows:
+            assert_equals(len(row), expected_length)
+
+    def _rows(self, table):
+        return self._rows_to_text(self._formatter.test_rows(table))
+
+    def _rows_to_text(self, rows):
+        return [[cell.content for cell in row] for row in rows]
+
+    def _create_test_table(self, additional_headers=[]):
+        table = TestCaseTable(None)
+        table.set_header(['Test Cases'] + additional_headers)
+        return table
+
+    def test_add_br_to_newlines(self):
+        original = """This is real new line:
+        here we have a single backslash n: \\n and here backslash + newline: \\\n and here bslash blash n \\\\n and bslash x 3 n \\\\\\n """
+        expected = 'This is real new line:\n        here we have a single backslash n: \\n<br>\nand here backslash + newline: \\\n and here bslash blash n \\\\n and bslash x 3 n \\\\\\n<br>\n'
+        assert_equals(HtmlCell(original).content, expected)
+
+    def test_no_br_to_newlines_without_whitespace(self):
+        original = r"Here there is no space after backslash-n: '\n'"
+        assert_equals(HtmlCell(original).content, original)
+
+    def test_no_br_to_double_backslashes(self):
+        original = r"Here there is double backslash-n: \\n "
+        assert_equals(HtmlCell(original).content, original)
+
+
+if __name__ == "__main__":
+    unittest.main()
