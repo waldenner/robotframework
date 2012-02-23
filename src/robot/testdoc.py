519251149fcbee7c3f26f5023ba97f7b8c7c10a7
Pekka Kl?rck <Pekka Kl?rck@localhost>
2012-02-23 16:36:26 +0200

    testdoc: script for creating data for template

---
+++ src/robot/testdoc.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+
+#  Copyright 2008-2011 Nokia Siemens Networks Oyj
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+USAGE = """Robot Framework Test Data Documentation Tool
+
+Usage:  testdoc.py [options] data_sources
+
+This tool generates a high level test documentation from a given test data.
+Generated documentation includes the names, documentations and other metadata
+of each test suite and test case, as well as the top-level keywords and their
+arguments. Most of the options accepted by this tool have exactly same
+semantics as same options have when executing test cases.
+
+Options:
+  -T --title title       Set the title of the generated documentation.
+                         Underscores in the title are converted to spaces.
+  -N --name name         Set the name of the top level test suite.
+  -D --doc document      Set the document of the top level test suite.
+  -M --metadata name:value *  Set metadata of the top level test suite.
+  -G --settag tag *      Set given tag(s) to all test cases.
+  -t --test name *       Include test cases by name.
+  -s --suite name *      Include test suites by name.
+  -i --include tag *     Include test cases by tags.
+  -e --exclude tag *     Exclude test cases by tags.
+  -h --help              Print this help.
+
+Examples:
+  $ testdoc.py mytestcases.html testdoc.html
+  $ testdoc.py --name smoke_test_plan --include smoke path/to/my_tests/ doc.html
+"""
+
+import sys
+import os
+import codecs
+
+if 'robot' not in sys.modules:
+    import pythonpathsetter   # running testdoc.py as script
+
+from robot import utils
+from robot.running import TestSuite, Keyword
+from robot.conf import RobotSettings
+from robot.parsing import populators
+from robot.reporting.htmlfilewriter import HtmlFileWriter, ModelWriter
+from robot.reporting.jsonwriter import JsonWriter
+
+populators.PROCESS_CURDIR = False
+
+
+class TestDoc(utils.Application):
+
+    def __init__(self):
+        utils.Application.__init__(self, USAGE, arg_limits=(2,), auto_version=False)
+
+    def main(self, args, title=None, **options):
+        datasources = args[0:-1]
+        outfile = os.path.abspath(args[-1])
+        suite = TestSuiteFactory(datasources, **options)
+        self._write_test_doc(suite, outfile, title)
+        self.console(outfile)
+
+    def _write_test_doc(self, suite, outfile, title):
+        output = codecs.open(outfile, 'w', 'UTF-8')
+        model_writer = TestdocModelWriter(output, suite, title)
+        HtmlFileWriter(output, model_writer).write('testdoc.html')
+        output.close()
+
+
+def TestSuiteFactory(datasources, **options):
+    if isinstance(datasources, basestring):
+        datasources = [datasources]
+    return TestSuite(datasources, RobotSettings(options))
+
+
+class TestdocModelWriter(ModelWriter):
+
+    def __init__(self, output, suite, title=None):
+        self._output = output
+        self._suite = suite
+        self._title = title.replace('_', ' ') if title else ''
+
+    def write(self, line):
+        self._output.write('<script type="text/javascript">' + os.linesep)
+        self._write_data()
+        self._output.write('</script>' + os.linesep)
+
+    def write_data(self):
+        json = JsonConverter().convert(self._suite)
+        json['title'] = self._title
+        JsonWriter(self._output).write_json('testdoc = ', json)
+
+
+class JsonConverter(object):
+
+    def convert(self, suite):
+        return self._convert_suite(suite)
+
+    def _convert_suite(self, suite):
+        return {
+            'source': suite.source,
+            'id': suite.id,
+            'name': suite.name,
+            'fullName': suite.longname,
+            'doc': suite.doc,
+            'metadata': dict(suite.metadata),
+            'numberOfTests': suite.get_test_count(),
+            'suites': self._convert_suites(suite),
+            'tests': self._convert_tests(suite),
+            'keywords': list(self._convert_keywords(suite))
+        }
+
+    def _convert_suites(self, suite):
+        return [self._convert_suite(s) for s in suite.suites]
+
+    def _convert_tests(self, suite):
+        return [self._convert_test(t) for t in suite.tests]
+
+    def _convert_test(self, test):
+        return {
+            'name': test.name,
+            'fullName': test.longname,
+            'id': test.id,
+            'doc': test.doc,
+            'tags': utils.normalize_tags(test.tags),
+            'timeout': self._get_timeout(test.timeout),
+            'keywords': list(self._convert_keywords(test))
+        }
+
+    def _convert_keywords(self, item):
+        if item.setup.name:
+            yield self._convert_keyword(item.setup, type='SETUP')
+        for kw in getattr(item, 'keywords', []):
+            yield self._convert_keyword(kw)
+        if item.teardown.name:
+            yield self._convert_keyword(item.teardown, type='TEARDOWN')
+
+    def _convert_keyword(self, kw, type=None):
+        return {
+            'name': kw._get_name(kw.name) if isinstance(kw, Keyword) else kw.name,
+            'arguments': ', '.join(kw.args),
+            'type': type or {'kw': 'KEYWORD', 'for': 'FOR'}[kw.type]
+        }
+
+    def _get_timeout(self, timeout):
+        try:
+            tout = utils.secs_to_timestr(utils.timestr_to_secs(timeout.string))
+        except ValueError:
+            tout = timeout.string
+        if timeout.message:
+            tout += ' :: ' + timeout.message
+        return tout
+
+
+def testdoc_cli(args):
+    TestDoc().execute_cli(args)
+
+
+if __name__ == '__main__':
+    testdoc_cli(sys.argv[1:])
---
+++ src/robot/writer/formatters.py
@@ -0,0 +1,142 @@
+#  Copyright 2008-2011 Nokia Siemens Networks Oyj
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import re
+
+from .aligners import FirstColumnAligner, ColumnAligner
+from .dataextractor import DataExtractor
+from .rowsplitter import RowSplitter
+
+
+class _DataFileFormatter(object):
+    _want_names_on_first_content_row = False
+
+    def __init__(self, column_count):
+        self._splitter = RowSplitter(column_count)
+        self._column_count = column_count
+        self._extractor = DataExtractor(self._want_names_on_first_content_row)
+
+    def empty_row_after(self, table):
+        return self._format_row([], table)
+
+    def format_header(self, table):
+        return self._format_row(self._header_for(table))
+
+    def format_table(self, table):
+        rows = self._extractor.rows_from_table(table)
+        if self._should_split_rows(table):
+            return self._split_rows(rows, table)
+        return [self._format_row(r, table) for r in rows]
+
+    def _should_split_rows(self, table):
+        if self._should_align_columns(table):
+            return False
+        return True
+
+    def _split_rows(self, rows, table):
+        for row in rows:
+            for r in self._splitter.split(row, self._is_indented_table(table)):
+                yield self._format_row(r, table)
+
+    def _should_align_columns(self, table):
+        return self._is_indented_table(table) and bool(table.header[1:])
+
+    def _is_indented_table(self, table):
+        return bool(table is not None and table.type in ['test case', 'keyword'])
+
+    def _escape_consecutive_whitespace(self, row):
+        return [re.sub('\s\s+(?=[^\s])',
+            lambda match: '\\'.join(match.group(0)), item.replace('\n', ' ')) for item in row]
+
+    def _format_row(self, row, table=None):
+        raise NotImplementedError
+
+    def _header_for(self, table):
+        raise NotImplementedError
+
+
+class TsvFormatter(_DataFileFormatter):
+
+    def _header_for(self, table):
+        return ['*%s*' % cell for cell in table.header]
+
+    def _format_row(self, row, table=None):
+        return self._pad(self._escape(row))
+
+    def _escape(self, row):
+        return self._escape_consecutive_whitespace(
+            self._escape_tabs(row))
+
+    def _escape_tabs(self, row):
+        return [c.replace('\t', '\\t') for c in row]
+
+    def _pad(self, row):
+        row = [cell.replace('\n', ' ') for cell in row]
+        return row + [''] * (self._column_count - len(row))
+
+
+class TxtFormatter(_DataFileFormatter):
+    _test_or_keyword_name_width = 18
+    _setting_and_variable_name_width = 14
+
+    def _format_row(self, row, table=None):
+        row = self._escape(row)
+        aligner = self._aligner_for(table)
+        if aligner:
+            return aligner.align_row(row)
+        return row
+
+    def _aligner_for(self, table):
+        if table and table.type in ['setting', 'variable']:
+            return FirstColumnAligner(self._setting_and_variable_name_width)
+        if self._should_align_columns(table):
+            return ColumnAligner(self._test_or_keyword_name_width, table)
+        return None
+
+    def _header_for(self, table):
+        header = ['*** %s ***' % table.header[0]] + table.header[1:]
+        aligner = self._aligner_for(table)
+        if aligner:
+            return aligner.align_row(header)
+        return header
+
+    def _escape(self, row):
+        return self._escape_consecutive_whitespace(
+            self._escape_empty_cells(row))
+
+    def _escape_empty_cells(self, row):
+        if not row:
+            return row
+        return [row[0]] + [c if c else '\\' for c in row[1:]]
+
+
+class PipeFormatter(TxtFormatter):
+
+    def _escape(self, row):
+        row = self._format_empty_cells(row)
+        return self._escape_consecutive_whitespace(self._escape_pipes(row))
+
+    def _format_empty_cells(self, row):
+        return ['  ' if not cell else cell for cell in row]
+
+    def _escape_pipes(self, row):
+        return [self._escape_pipes_from_cell(cell) for cell in row]
+
+    def _escape_pipes_from_cell(self, cell):
+        cell = cell.replace(' | ', ' \\| ')
+        if cell.startswith('| '):
+            cell = '\\' + cell
+        if cell.endswith(' |'):
+            cell = cell[:-1] + '\\|'
+        return cell
