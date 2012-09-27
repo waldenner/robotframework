6ae3831130a71573399766d9aa6d0a77782c0f59
Pekka Kl?rck <Pekka Kl?rck@localhost>
2012-09-27 10:58:15 +0300

    XML lib atests: Fixed on ET 1.3 (i.e. Python 2.7) and cleaned up a little.

---
+++ atest/resources/atest_variables.py
@@ -0,0 +1,11 @@
+from os.path import abspath, dirname, join
+import os
+import tempfile
+import robot
+
+__all__ = ['ROBOTPATH', 'ROBOT_VERSION', 'DATADIR', 'WINDOWS']
+
+ROBOTPATH = dirname(abspath(robot.__file__))
+ROBOT_VERSION = robot.version.get_version()
+DATADIR = join(dirname(abspath(__file__)), '..', 'testdata')
+WINDOWS = os.sep == '\\'
