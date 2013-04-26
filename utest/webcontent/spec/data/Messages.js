08c74ebab43c15560698150f3d54b7963abee04f
Mika H?nninen <mika.hanninen@gmail.com>
2013-04-26 14:31:28 +0300

    first draft version of Process libdoc

---
+++ utest/webcontent/spec/data/Messages.js
@@ -0,0 +1,20 @@
+window.messagesOutput = {};
+
+window.messagesOutput["suite"] = [1,2,3,0,[],[1,0,27],[],[[4,0,1,0,[],[1,23,4],[[0,5,0,6,7,[1,24,0],[],[[24,2,8]]],[0,5,0,6,9,[1,24,0],[],[[24,2,9]]],[0,5,0,6,10,[1,24,1],[],[[25,3,12]]],[0,13,0,14,15,[1,25,0],[],[[25,2,16],[25,0,17]]],[0,5,0,6,18,[1,26,0],[],[[26,0,19],[26,1,20],[26,0,21]]],[0,5,0,6,22,[1,26,0],[],[[26,0,23],[26,0,24],[26,0,21]]],[0,13,0,14,25,[1,27,0],[],[[27,0,26],[27,2,27]]]]]],[],[1,1,1,1]];
+
+window.messagesOutput["strings"] = [];
+
+window.messagesOutput["strings"] = window.messagesOutput["strings"].concat(["*","*Messages","*/home/mkorpela/workspace/robot/utest/webcontent/spec/data/Messages.txt","*Messages.txt","*Test with messages","*BuiltIn.Log","*<p>Logs the given message with the given level.</p>","*&lt;h1&gt;html&lt;/h1&gt;, HTML","*<h1>html</h1>","*infolevelmessage","*warning, WARN","*s1-t1-k3","*warning","*BuiltIn.Set Log Level","*<p>Sets the log threshold to the specified level and returns the old level.</p>","*TRACE","*Log level changed from INFO to TRACE","*Return: 'INFO'","*debugging, DEBUG","*Arguments: [ u'debugging' | u'DEBUG' ]","*debugging","*Return: None","*tracing, TRACE","*Arguments: [ u'tracing' | u'TRACE' ]","*tracing","*INFO","*Arguments: [ u'INFO' ]","*Log level changed from TRACE to INFO"]);
+
+window.messagesOutput["generatedTimestamp"] = "20130404 15:36:19 GMT +03:00";
+
+window.messagesOutput["errors"] = [[25,3,12,11]];
+
+window.messagesOutput["stats"] = [[{"elapsed":"00:00:00","fail":0,"label":"Critical Tests","pass":1},{"elapsed":"00:00:00","fail":0,"label":"All Tests","pass":1}],[],[{"elapsed":"00:00:00","fail":0,"id":"s1","label":"Messages","name":"Messages","pass":1}]];
+
+window.messagesOutput["generatedMillis"] = -820;
+
+window.messagesOutput["baseMillis"] = 1365078979820;
+
+window.settings = {"background":{"fail":"DeepPink"},"logURL":"log.html","reportURL":"report.html"};
+
