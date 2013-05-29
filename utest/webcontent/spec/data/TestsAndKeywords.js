f62ef9ddf808edaea615e78d484da79af067ac54
jussi.ao.malinen <jussi.ao.malinen@localhost>
2013-05-29 16:39:49 +0300

    document that set test message is now overrridden by any failure message

---
+++ utest/webcontent/spec/data/TestsAndKeywords.js
@@ -0,0 +1,20 @@
+window.testsAndKeywordsOutput = {};
+
+window.testsAndKeywordsOutput["suite"] = [1,2,3,0,[],[1,0,6],[],[[4,0,1,0,[],[1,0,3],[[0,5,0,0,0,[1,0,1],[[0,6,0,7,0,[1,1,0],[],[]]],[]],[0,8,0,0,0,[1,1,1],[[0,6,0,7,0,[1,1,1],[],[]]],[]],[0,9,0,0,0,[1,2,0],[[0,6,0,7,0,[1,2,0],[],[]]],[]],[0,10,0,0,0,[1,2,1],[[0,6,0,7,0,[1,3,0],[],[]]],[]]]],[11,0,1,0,[],[1,3,1],[[0,6,0,7,0,[1,4,0],[],[]]]],[12,0,1,0,[],[1,4,1],[[0,6,0,7,0,[1,5,0],[],[]]]],[13,0,1,0,[],[1,5,1],[[0,6,0,7,0,[1,5,1],[],[]]]]],[],[4,4,4,4]];
+
+window.testsAndKeywordsOutput["strings"] = [];
+
+window.testsAndKeywordsOutput["strings"] = window.testsAndKeywordsOutput["strings"].concat(["*","*TestsAndKeywords","*/Users/mikhanni/koodi/robotframework/utest/webcontent/spec/data/TestsAndKeywords.txt","*utest/webcontent/spec/data/TestsAndKeywords.txt","*Test 1","*kw1","*BuiltIn.No Operation","*<p>Does absolutely nothing.</p>","*kw2","*kw3","*kw4","*Test 2","*Test 3","*Test 4"]);
+
+window.testsAndKeywordsOutput["generatedTimestamp"] = "20130529 14:41:30 GMT +03:00";
+
+window.testsAndKeywordsOutput["errors"] = [];
+
+window.testsAndKeywordsOutput["stats"] = [[{"elapsed":"00:00:00","fail":0,"label":"Critical Tests","pass":4},{"elapsed":"00:00:00","fail":0,"label":"All Tests","pass":4}],[],[{"elapsed":"00:00:00","fail":0,"id":"s1","label":"TestsAndKeywords","name":"TestsAndKeywords","pass":4}]];
+
+window.testsAndKeywordsOutput["generatedMillis"] = -192;
+
+window.testsAndKeywordsOutput["baseMillis"] = 1369827690192;
+
+window.settings = {"background":{"fail":"DeepPink"},"logURL":"log.html","reportURL":"report.html"};
+
